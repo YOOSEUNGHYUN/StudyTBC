96aee95a232a5be6955dc02abfc586bc9b9ded41
SEUNGHYUN <ysh7733@naver.com>
2023-03-28 22:06:44 +0900

    study program's structure and comments

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -3,5 +3,18 @@ using namespace std;
 
 int main()
 {
+	int sight;
+	int n_items;
+	int price_per_item;
+
+	//물약 먹어서 시야 거리가 0
+	sight = 0;
+
+	n_items = 100;
+	price_per_item = 20;
+
+	// 1 + 1 event
+	int cost = n_items / 2 * price_per_item;
+
 	return 0;
 }
