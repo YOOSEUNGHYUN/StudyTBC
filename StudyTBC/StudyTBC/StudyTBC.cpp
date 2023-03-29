05ae44aee9079dbf4dfd76f37ba8e00b53db3b6d
SEUNGHYUN <ysh7733@naver.com>
2023-03-29 22:20:56 +0900

    First meeting with a function

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,28 +1,28 @@
 #include <iostream> // cout, cin, endl, ...
-#include <cstdio> // printf
 using namespace std;
+
+int addTwoNumbers(int a, int b)
+{
+	int sum = a + b;
+	cout << sum << endl;
+
+	return 0;
+}
 
 int main()
 {
-	/*int x = 1024;
-	double pi = 3.141592;
+	int a;
+	int b;
 
-	cout << "I love this lecture!\n\n\n\n";
-	cout << "x is " << x << " pi is " << pi << endl;
+	cin >> a;
+	cin >> b;
 
-	cout << "abc" << "\t" << "def" << endl;
-	cout << "ab" << "\t" << "cdef" << endl;
-	cout << "abcde" << "\t" << "fghijk" << endl;
-
-	cout << "\a";*/
-
-	int x = 1;
-
-	cout << "Before your input, x was " << x << endl;
-
-	cin >> x;
-
-	cout << "Your input is " << x << endl;
-
+	while(1)
+	{
+		addTwoNumbers(a, b);
+		cin >> a;
+		cin >> b;
+	}
+	
 	return 0;
 }
