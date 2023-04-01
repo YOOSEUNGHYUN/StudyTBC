ae2b7373c77090eeeb61e63dc9a51af3daed5b86
SEUNGHYUN <ysh7733@naver.com>
2023-04-01 14:09:57 +0900

    Study namespace

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
-#include "my_work.h"
-#include "add.h"
-
 using namespace std;
+
+namespace MySpace1
+{
+	namespace InnerSpace
+	{
+		int my_function()
+		{
+			return 0;
+		}
+	}
+
+	int doSomething(int a, int b)
+	{
+		return a + b;
+	}
+
+}
+
+namespace MySpace2
+{
+	int doSomething(int a, int b)
+	{
+		return a * b;
+	}
+}
 
 int main()
 {
-	doSomething();
+	using namespace MySpace1::InnerSpace;
+	my_function();
+
+	/*cout << MySpace1::doSomething(3, 4) << endl;
+	cout << MySpace2::doSomething(3, 4) << endl;*/
 
 	return 0;
 }
