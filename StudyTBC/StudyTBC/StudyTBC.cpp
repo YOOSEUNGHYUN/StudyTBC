b299841d6ff48e23b7a092bd8d93e65e9f4d6e21
SEUNGHYUN <ysh7733@naver.com>
2023-04-02 14:10:28 +0900

    Study Fundamental Data Types

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-
-#define LIKE_APPLE
-
-void doSomething(); //forward declaration
 
 int main()
 {
-	doSomething();
+	bool bValue = true;
+	char chValue = 65;
+	float fValue = 3.141592f;
+	double dValue = 3.141592;
+
+	int a (123);  // direct initialization
+	int b{ 123 }; // uniform initialization
+
+	int i = (int)3.141592;
+	int z((int)3.14);
+	int y{ 4 }; 
+
+	//int k = 0, l = 456, m = 123;
+	int k = 0, l(456), m{ 123 };
+
+	auto aValue = 3.141592;
+	auto aValue2 = 3.141592f;
+
+	cout << sizeof(aValue) << endl;
+	cout << sizeof(aValue2) << endl;
+	cout << sizeof(bool) << endl;
 
 	return 0;
 }
