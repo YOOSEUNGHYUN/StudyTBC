14f21926769afe76a7ba9041a8113bb1538ee8b4
SEUNGHYUN <ysh7733@naver.com>
2023-04-03 12:02:11 +0900

    Study Fixed width Integers

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
-#include <cmath>
-#include <limits>
+//#include <cstdint>
 using namespace std;
 
 int main()
 {
-	short       s = 1; // 2 bytes = 2 * 8 bits = 16 bits
-	unsigned int         i = 1;
+	int16_t i(5);
+	int8_t myint = 65;
 
-	cout << pow(2, sizeof(unsigned int) * 8) -1 << endl;
-	cout << numeric_limits<unsigned int>::max() << endl;
-	cout << numeric_limits<unsigned int>::min() << endl;
-	cout << numeric_limits<unsigned int>::lowest() << endl;
+	cout << myint << endl;
 
-	i = numeric_limits<unsigned int>::max();
-
-	cout << "unsigned int max() : " << i << endl;
-
-	i = i + 1;
-
-	cout << i << endl;
+	int_fast8_t fi(5);
+	int_least64_t fl(5);
 
 	return 0;
 }
