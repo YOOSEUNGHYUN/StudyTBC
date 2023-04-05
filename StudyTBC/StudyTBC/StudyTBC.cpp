e31064b7551bfb0b69deb80770f0c917198c5aad
SEUNGHYUN <ysh7733@naver.com>
2023-04-05 20:46:45 +0900

    Study Symbolic Constants

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
+#include "MY_CONSTANTS.h"
 using namespace std;
 
 int main()
 {
-	// Decimal(10진수)      : 0 1 2 3 4 5 6 7 8 9 10
-	// Octal(8진수)         : 0 1 2 3 4 5 6 7 10 11 12 13
-	// Hexa Decimal(16진수) : 0 1 2 3 4 5 6 7 8 9 A B C D E F 10
+	double radius;
+	cin >> radius;
 
-	const int price_per_item = 10;
-	int num_items = 123;
-	int price = num_items * price_per_item; 
+	double circumference = 2.0 * radius * constants::pi;
 
 	return 0;
 }
