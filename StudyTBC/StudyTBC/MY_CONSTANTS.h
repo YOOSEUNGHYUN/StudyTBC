612bb682ed78371377fa83321ed760a0f3086e7c
SEUNGHYUN <ysh7733@naver.com>
2023-04-05 20:49:22 +0900

    Study Symvolic Constants again

--- StudyTBC/StudyTBC/MY_CONSTANTS.h
+++ StudyTBC/StudyTBC/MY_CONSTANTS.h
@@ -1,1 +1,10 @@
 #pragma once
+
+namespace constants
+{
+	// constexpr는 컴파일 타임에 확실히 결정되어야 하는 상수다.
+	constexpr double pi(3.141592);
+	constexpr double avogadro(6.0221413e23);
+	constexpr double moon_gravity(9.8 / 6.0);
+
+}
