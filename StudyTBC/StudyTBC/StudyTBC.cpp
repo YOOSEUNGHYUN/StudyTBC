af36531d9975b49cbf73c0a564e8e5ff7990ca8b
SEUNGHYUN <ysh7733@naver.com>
2023-04-05 17:27:11 +0900

    Study char type

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
+#include <limits>
+
 using namespace std;
 
 int main()
 {
+	char c1(72);
+	char c2(89);
+	char c3(79);
+	char c4(78);
+	char c5(73);
+	int ic1(72);
+	int ic2(69);
+	int ic3(76);
+	int ic4(76);
+	int ic5(79);
 
-	int a;
-	cin >> a;
+	cin >> ic1;
+	cin >> ic2;
+	cin >> ic3;
+	cin >> ic4;
+	cin >> ic5;
+	
+	cout << static_cast<char>(ic1) 
+		<< static_cast<char>(ic2) 
+		<< static_cast<char>(ic3) 
+		<< static_cast<char>(ic4) 
+		<< static_cast<char>(ic5) << endl;
 
-	if (a == 0)
-	{
-		cout << "니가 입력한 수는 0이다." << endl;
-	}
-
-	else if (a % 2 == 1)
-	{
-		cout << "니가 입력한 수는 홀수다." << endl;
-	}
-
-	else if (a % 2 == 0)
-	{
-		cout << " 니가 입력한 숫자는 짝수다." << endl;
-	}
+	cout << c1 << c2 << c3 << c4 << c5 << endl;
 
 	return 0;
 }
