24487f10f9990b7e76bdcf79b690b89777d2f72c
SEUNGHYUN <ysh7733@naver.com>
2023-04-06 19:51:35 +0900

    Study Operator Precedence and Associativity

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
-#include "MY_CONSTANTS.h"
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	double radius;
-	cin >> radius;
+	bool a = false;
+	bool b = true;
+	bool c = false;
+	bool d = false;
 
-	double circumference = 2.0 * radius * constants::pi;
+	cout << boolalpha;
+	cout << a << " " << b << " " << c << " " << d << endl;
+	 
+	// <<,>> 이 &&보다 우선 순위가 높아서 cout << a 또는 cout << (a || b) 까지만 나온다. 
+	cout << a || b && c || d;
+	cout << (a || b) && (c || d);
+	cout << a || (b && c) || d;
+	cout << (a || b) && c || d;
+	cout << a || b && (c || d);
+
+	cout << '\t' << (a || b && c || d) << endl;
 
 	return 0;
 }
