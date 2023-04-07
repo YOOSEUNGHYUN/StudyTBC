4af31530d78c460337235c8f1bec408a751c7aad
SEUNGHYUN <ysh7733@naver.com>
2023-04-07 10:27:24 +0900

    Study Arithmetic Operators

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
 int main()
 {
-	bool a = false;
-	bool b = true;
-	bool c = false;
-	bool d = false;
+	int x = 7;
+	int y = 4;
 
-	cout << boolalpha;
-	cout << a << " " << b << " " << c << " " << d << endl;
-	 
-	// <<,>> 이 &&보다 우선 순위가 높아서 cout << a 또는 cout << (a || b) 까지만 나온다. 
-	cout << a || b && c || d;
-	cout << (a || b) && (c || d);
-	cout << a || (b && c) || d;
-	cout << (a || b) && c || d;
-	cout << a || b && (c || d);
+	int z = x;
+	z += y; // z = z + y;
+	cout << z << endl;
 
-	cout << '\t' << (a || b && c || d) << endl;
+	z -= y; // z = z - y;
+	cout << z << endl;
+
+	z *= y; // z = z * y;
+	cout << z << endl;
+
+	z /= y; // z = z / y;
+	cout << z << endl;
+
+	z %= y; // z = z % y;
+	cout << z << endl;
 
 	return 0;
 }
+
 
