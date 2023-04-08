6b2f920cc02221eedfa0e3cfe6daf11457fb30a3
SEUNGHYUN <ysh7733@naver.com>
2023-04-08 22:58:09 +0900

    Study Relational Operators

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-
 int main()
 {
-	int x = 5;
+	double d1(100 - 99.99);  // 0.001
+	double d2(10 - 9.99);    // 0.001
 
-	cout << ((x % 2 == 0) ? "even" : "odd") << endl;
+	const double epsilon = 1e-16;
+
+	if (std::abs(d1 - d2) < epsilon)
+		cout << "Approximately equal" << endl;
+	else
+		cout << "Not equal " << endl;
 
 	return 0;
 }
