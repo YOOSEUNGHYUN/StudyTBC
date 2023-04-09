b9be920856ab15e57c38b2d34a69ccb2649b4158
SEUNGHYUN <ysh7733@naver.com>
2023-04-09 15:17:49 +0900

    Study Logical Operators

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -6,15 +6,32 @@ using namespace std;
 
 int main()
 {
-	double d1(100 - 99.99);  // 0.001
-	double d2(10 - 9.99);    // 0.001
+	/*bool x = true;
+	bool y = true;
+	
+	cout << boolalpha;
+	cout << (true && true) || false;
+	cout << (false && true) || true;
+	cout << (false && true) || false || true;
+	cout << (14 > 13 || 2 > 1) && (9 > 1);
+	cout << !(2314123 > 2 || 123123 > 2387) << endl;
+	cout << true || false;
+	cout << (false && true);*/
 
-	const double epsilon = 1e-16;
+	bool v1 = true;
+	bool v2 = false;
+	bool v3 = false;
 
-	if (std::abs(d1 - d2) < epsilon)
-		cout << "Approximately equal" << endl;
-	else
-		cout << "Not equal " << endl;
+	bool r1 = v1 || v2 && v3;
+	bool r2 = (v1 || v2) && v3;
+	bool r3 = v1 || (v2 && v3);
+
+
+	cout << boolalpha;
+	cout << r1 << endl;
+	cout << r2 << endl;
+	cout << r3 << endl;
+
 
 	return 0;
 }
