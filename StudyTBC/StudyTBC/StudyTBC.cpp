40dc18182d586d9d514571d26cbcdeff3e5359a0
SEUNGHYUN <ysh7733@naver.com>
2023-04-14 18:45:42 +0900

    Study local variable scope and duration

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
-#include <bitset>
 
 using namespace std;
 
+namespace work1
+{
+	namespace work11
+	{ 
+		int a = 1;
+		void doSomething()
+		{
+			a += 3;
+			cout << a << endl;
+		}
+	}
+}
+
+namespace work2
+{
+	int a = 1;
+	void doSomething()
+	{
+		a += 5;
+		cout << a << endl;
+	}
+}
+
 int main()
 {
-	unsigned char option_viewd = 0x01;
-	unsigned char option_edited = 0x02;
-	unsigned char option_liked = 0x04;
-	unsigned char option_shared = 0x08;
-	unsigned char option_deleted = 0x80;
+	work1::a;
+	work1::work11::doSomething();
 
-	unsigned char my_article_flags = 0;
+	work2::a;
+	work2::doSomething();
 
-	
-	//기사를 봤을 때
-	if (my_article_flags | option_viewd)
-	{
-		my_article_flags |= option_viewd;
-	}
-
-	//기사의 좋아요를 다시 클릭했을 때
-	if (my_article_flags & option_liked)
-	{
-		my_article_flags ^= option_liked;
-	}
-	else //기사의 좋아요를 클릭했을 때
-	{
-		my_article_flags |= option_liked;
-	}
-
-	//본 기사만 삭제할 때
-	if (my_article_flags & option_viewd)
-	{
-		my_article_flags |= option_deleted;
-	}
-	
 	return 0;
 }
 
