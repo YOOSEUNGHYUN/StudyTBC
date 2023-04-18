9a0a23d3bca6b296a89894f09bdbd178c9b19561
SEUNGHYUN <ysh7733@naver.com>
2023-04-18 22:58:57 +0900

    Study std string

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -6,21 +6,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Your age ? : ";
-	int age;
-	cin >> age;
-	//std::getline(std::cin, age);
-
-	//cin.ignore(32767, '\n');
-	cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-	cout << "Your name ? : ";
-	string name;
-	//cin >> name;
-	std::getline(std::cin, name);
-
-	cout << "Name is : " << name << endl;
-	cout << "Age is : " << age << endl;
+	string a("Hello, World");
+	
+	cout << a.length() << endl;
 
 	return 0;
 }
