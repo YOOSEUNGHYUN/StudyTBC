b9962eb2efb04233568e4a96a6783a9c1b0c5423
SEUNGHYUN <ysh7733@naver.com>
2023-04-19 22:09:08 +0900

    Study enum class

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
-#include <typeinfo>
-#include <string>
 
 using namespace std;
 
-enum Color //user-defined data types 사용자 정의 자료형
-{
-	COLOR_BLACK,
-	COLOR_RED,
-	COLOR_BLUE,
-	COLOR_GREEN,
-	COLOR_SKYBLUE,
-};
-
 int main()
 {
-	int color_id = COLOR_RED;
-	Color my_color = static_cast<Color>(3);
-
-	//cin >> my_color;
-	/*cout << color_id << endl;
-
-	int in_number;
-	cin >> in_number;*/
-	string str_input;
-	
-	getline(cin, str_input);
-
-	if (str_input == "COLOR_BLACK")
+	enum class Color
 	{
-		my_color = static_cast<Color>(0);
+		RED,
+		BLUE
+	};
+
+	enum class Fruit
+	{
+		BANANA,
+		APPLE,
+	};
+
+	Color color1 = Color::RED;
+	Color color2 = Color::BLUE;
+
+	if (color1 == color2)
+	{
+		cout << "Same Color" << endl;
+	}
+	else
+	{
+		cout << "Not Same Color" << endl;
 	}
 
 	return 0;
