6f8b09f33e02582b7486e4bbe3440e84f071807e
SEUNGHYUN <ysh7733@naver.com>
2023-04-19 21:57:06 +0900

    Study enumerated types

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <typeinfo>
 #include <string>
-#include <limits>
 
 using namespace std;
 
+enum Color //user-defined data types 사용자 정의 자료형
+{
+	COLOR_BLACK,
+	COLOR_RED,
+	COLOR_BLUE,
+	COLOR_GREEN,
+	COLOR_SKYBLUE,
+};
+
 int main()
 {
-	string a("Hello, World");
+	int color_id = COLOR_RED;
+	Color my_color = static_cast<Color>(3);
+
+	//cin >> my_color;
+	/*cout << color_id << endl;
+
+	int in_number;
+	cin >> in_number;*/
+	string str_input;
 	
-	cout << a.length() << endl;
+	getline(cin, str_input);
+
+	if (str_input == "COLOR_BLACK")
+	{
+		my_color = static_cast<Color>(0);
+	}
 
 	return 0;
 }
+
 
 
