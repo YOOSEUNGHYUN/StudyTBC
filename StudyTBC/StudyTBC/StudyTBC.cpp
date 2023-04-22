24bae0bd75fa7d3b726f200eea09170ecfada9a5
SEUNGHYUN <ysh7733@naver.com>
2023-04-22 21:09:05 +0900

    Study switch case

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,15 +2,32 @@
 
 using namespace std;
 
+
 int main()
 {
 	int x;
 	cin >> x;
 
-	if (x = 0) // x = 0; if(x)
-		cout << x << endl;
+	switch (x)
+	{
+		case 0:
+		{
+			int y = 5;
+			y = y + x;
+			cout << y << endl;
+			break;
+		}
+		case 1:
+		{
+			int y = 5;
+			y = y - x;
+			cout << y << endl;
+			break;
+		}
 
-	cout << x << endl;
+		default:
+			cout << "Undefined input " << x << endl;
+	}
 
 	return 0;
 }
