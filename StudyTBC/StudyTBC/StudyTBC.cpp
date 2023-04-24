dac06051af57747e375caf53c4ecfa3215bde0c7
SEUNGHYUN <ysh7733@naver.com>
2023-04-24 22:55:25 +0900

    study do while

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -4,21 +4,19 @@ using namespace std;
 
 int main()
 {
-	cout << "While-loop test" << endl;
+	int selection; // must be declared outside do/while loop
 
-	int outer_count=1;
-
-	while (outer_count <= 5)
+	do
 	{
-		int inner_count = 1;
-		while (inner_count <= outer_count)
-		{
-			cout << inner_count++ << " ";
-		}
+		cout << "1. add" << endl;
+		cout << "2. sub" << endl;
+		cout << "3. mult" << endl;
+		cout << "4. div" << endl;
+		cin >> selection;
+	} while (selection <= 0 || selection >= 5);
 
-		cout << endl;
-		++outer_count;
-	}
+	cout << "You selected " << selection << endl;
+	
 	return 0;
 }
 
