93f550fcea754caa459218605eea49b02b3c647d
SEUNGHYUN <ysh7733@naver.com>
2023-04-25 23:25:57 +0900

    Study break continue

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,23 +2,15 @@
 
 using namespace std;
 
-int pow(int base, int exponent)
-{
-	int result = 1;
-
-	for (int count = 0; count < exponent; ++count)
-		result *= base;
-
-	return result;
-}
-
 int main()
 {
-	for(int j = 0; j <9; ++j)
-		for (int i = 0; i < 9; ++i)
-		{
-			cout << i << " " << j << endl;
-		}
+	int count = 0;
+	while (true)
+	{
+		cout << count << endl;
+		if (count == 10) break;
+		count++;
+	}
 
 	return 0;
 }
