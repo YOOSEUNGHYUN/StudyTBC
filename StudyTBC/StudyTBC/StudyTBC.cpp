2b9eb6b8445bf17dbd95b085057a8a199965ab82
SEUNGHYUN <ysh7733@naver.com>
2023-04-26 22:07:42 +0900

    Study making random number

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,39 +1,28 @@
 #include <iostream>
+#include <cstdlib> // std::rand(), std::srand()
 
 using namespace std;
 
-void breakOrReturn()
+unsigned int PRNG() // Pseudo Random Number Generator
 {
-	while (true)
-	{
-		char ch;
-		cin >> ch;
+	static unsigned int seed = 5523; // seed number
 
-		if (ch == 'b')
-			break;
-		if (ch == 'r')
-			return;
-	}
+	seed = 825379 * seed + 2396403;
 
-	cout << "Hello" << endl;
+	return seed % 32768;
 }
 
 int main()
 {
-	int count(0);
-	bool escape_flag = false;
-	while (true)
+	for (int count = 1; count <= 100; ++count)
 	{
-		char ch;
-		cin >> ch;
+		cout << PRNG() << "\t";
 
-		cout << ch << " " << count++ << endl;
-
-		if (ch == 'x')
-			break;
+		if (count % 5 == 0) cout << endl;
 	}
 
 	return 0;
 }
+
 
 
