362c1aee80c4785e70345c24653a99d613774c69
SEUNGHYUN <ysh7733@naver.com>
2023-04-27 23:46:42 +0900

    Study get Random Number

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <cstdlib> // std::rand(), std::srand()
+#include <ctime> //std::time()
 
 using namespace std;
 
-unsigned int PRNG() // Pseudo Random Number Generator
+//unsigned int PRNG() // Pseudo Random Number Generator
+//{
+//	static unsigned int seed = 5523; // seed number
+//
+//	seed = 825379 * seed + 2396403;
+//
+//	return seed % 32768;
+//
+
+int getRandomNumber(int min, int max)
 {
-	static unsigned int seed = 5523; // seed number
+	static const double fraction = 1.0 / (RAND_MAX + 1.0);
 
-	seed = 825379 * seed + 2396403;
-
-	return seed % 32768;
+	return min + static_cast<int>((max - min + 1) * (std::rand() * fraction));
 }
 
 int main()
 {
 	for (int count = 1; count <= 100; ++count)
 	{
-		cout << PRNG() << "\t";
+		cout << getRandomNumber(5,8) << "\t";
 
 		if (count % 5 == 0) cout << endl;
 	}
