dd63c4ce62ab819d606189c7181d0493f3b56866
SEUNGHYUN <ysh7733@naver.com>
2023-04-30 23:51:09 +0900

    Study array 2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-
 using namespace std;
+
+#define NUM_STUDENTS 100000
 
 int main()
 {
-	int one_student_score; // 1 variable
-	int student_scores[5]; // 5 int
+	const int num_students = 20;
+	//cin >> num_students;
 
-	cout << sizeof(one_student_score) << endl;
-	cout << sizeof(student_scores) << endl;
+	int students_scores[num_students];
+
+	cout << &students_scores << endl;
+	cout << &students_scores[0] << endl;
+	cout << &students_scores[1] << endl; 
+	cout << &students_scores[2] << endl;
+	cout << &students_scores[3] << endl;
+
+	cout << sizeof(students_scores) << endl;
 
 	return 0;
 }
