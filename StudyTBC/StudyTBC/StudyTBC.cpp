0d70ecf630ab221a0cd36e6b90b9139ea7926fd9
SEUNGHYUN <ysh7733@naver.com>
2023-05-01 23:11:47 +0900

    Study cin again

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,26 +1,68 @@
 #include <iostream>
+
 using namespace std;
+
+int getInt()
+{
+	while(true)
+	{ 
+		cout << "Enter an integer number : ";
+		int x;
+		cin >> x;
+
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+			cout << "Invalid number, please try again" << endl;
+		}
+		else
+		{
+			std::cin.ignore(32767, '\n');
+			return x;
+		}
+	}
+}
+
+char getOperator()
+{
+	while(true)
+	{ 
+		cout << "Enter an operator (+,-,*,/) : ";
+		char op;
+		cin >> op;
+		std::cin.ignore(32767, '\n');
+
+		if (op == '+' || op == '-' || op == '*' || op == '/')
+			return op;
+		else
+			cout << "Invalid operator, please try again " << endl;
+	}
+}
+
+void printResult(int x, char op, int y)
+{
+	if (op == '+') cout << x + y << endl;
+	else if (op == '-') cout << x - y << endl;
+	else if (op == '*') cout << x * y << endl;
+	else if (op == '/') cout << x / y << endl;
+	else
+	{
+		cout << "Invalid operator" << endl;
+	}
+}
 
 int main()
 {
-	//const int num_students = 5;
-	int scores[] = { 84, 92, 76, 81, 56 };
+	int  x = getInt();
+	char op = getOperator();
+	int  y = getInt();
 
-	const int num_students = sizeof(scores) / sizeof(int);
 
-	int max_score = 0;
-	int total_score = 0;
-
-	for (int i = 0; i < num_students; ++i)
-	{
-		total_score += scores[i];
-		//max_score = (max_score < scores[i]) ? scores[i] : max_score;
-		if (max_score < scores[i]) max_score = scores[i];
-	}
-
-	double avg_score = static_cast<double>(total_score) / num_students;
-
-	cout << max_score << endl;
+	printResult(x, op, y);
 
 	return 0;
+
+	
+
 }
