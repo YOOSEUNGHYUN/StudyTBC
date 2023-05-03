a8a4af1d77b271dc79011f9f34978aa7fafbed11
SEUNGHYUN <ysh7733@naver.com>
2023-05-03 14:09:41 +0900

    Study Multi-dimensional Array

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,60 +2,20 @@
 
 using namespace std;
 
-void printArray(const int array[], const int length)
-{
-	for (int index = 0; index < length; ++index)
-		cout << array[index] << " ";
-	cout << endl;
-}
-
 int main()
 {
-	/*             value     index
-	3 5 2 1 4        1         3
-	1 5 2 3 4		 2		   2
-	1 2 5 3 4		 3		   3
-	1 2 3 5 4		 4		   4
-	1 2 3 4 5
-	*/
+	const int num_rows = 3;
+	const int num_columns = 5;
 
-	const int length = 5;
-
-	int array[length] = { 3, 5, 2, 1, 4 };
-
-	printArray(array, length);
-
-	// swap
-	/*int temp = array[0];
-	array[0] = array[1];
-	array[1] = temp;*/
-
-	for (int startIndex = 0; startIndex < length - 1; ++startIndex)
+	for (int row = 0; row < num_rows; ++row)
 	{
-		int smallestIndex = startIndex;
+		for (int col = 0; col < num_columns; ++col)
+			cout << '[' << row << ']' << '[' << col << ']' << '\t';
 
-		for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
-		{
-			if (array[smallestIndex] > array[currentIndex])
-			{
-				smallestIndex = currentIndex;
-			}
-		}
-
-		//swap two values in the array
-		//std::swap(array[smallestIndex],array[startIndex]);
-		{
-			int temp = array[smallestIndex];
-			array[smallestIndex] = array[startIndex];
-			array[startIndex] = temp;
-		}
-
-		printArray(array, length);
+		cout << endl;
 	}
 
+	cout << endl;
 
 	return 0;
-
-	
-
 }
