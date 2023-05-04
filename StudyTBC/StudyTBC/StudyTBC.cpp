bc5825da5413dfe4503e9cb1bf8d2a550ba32c4f
SEUNGHYUN <ysh7733@naver.com>
2023-05-04 22:07:36 +0900

    Study c style array string

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
-	const int num_rows = 3;
-	const int num_columns = 5;
+	char source[] = "Copy this!";
+	char dest[50];
+	strcpy_s(dest, 50, source);
 
-	for (int row = 0; row < num_rows; ++row)
-	{
-		for (int col = 0; col < num_columns; ++col)
-			cout << '[' << row << ']' << '[' << col << ']' << '\t';
+	//cout << source << endl;
+	//cout << dest << endl;
 
-		cout << endl;
-	}
-
-	cout << endl;
+	//strcat()
+	//strcmp()
 
 	return 0;
 }
