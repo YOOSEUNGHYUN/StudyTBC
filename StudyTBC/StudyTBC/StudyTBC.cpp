af5058d3a04da2ead7dcbc2031d29b37b6cb8ef4
SEUNGHYUN <ysh7733@naver.com>
2023-05-05 20:29:56 +0900

    study array and for again

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,32 +2,32 @@
 
 using namespace std;
 
-#define NUM_STUDENTS 1000000
-
-void doSomething(int students_scores[20])
-{
-	cout << (int)&students_scores << endl;
-	cout << (int)&students_scores[0] << endl;
-	cout << students_scores[0] << endl;
-	cout << students_scores[1] << endl;
-	cout << students_scores[2] << endl;
-	cout << sizeof(students_scores) << endl;
-}
-
 int main()
 {
-	const int num_students = 20;
+	//const int num_students = 5;
+	int scores[] = { 84, 92, 76, 81, 56 };
 
-	int students_scores[num_students] = { 1, 2, 3, 4, 5, };
-	
-	cout << (int)students_scores << endl;
-	cout << (int)&students_scores << endl;
-	cout << students_scores[0] << endl;
-	cout << students_scores[1] << endl;
-	cout << students_scores[2] << endl;
-	cout << sizeof(students_scores) << endl;
-	
-	doSomething(students_scores);
+	const int num_students = sizeof(scores) / sizeof(int);
+
+	int total_score = 0;
+
+	for (int i = 0; i < num_students; ++i)
+	{
+		total_score = scores[i];
+	}
+
+	double avg_score = static_cast<double>(total_score) / num_students;
+
+	/*int score0 = 84;
+	int score1 = 92;
+	int score2 = 76;
+	int score3 = 81;
+	int score4 = 56;
+
+	int total_score = score0 + score1 + score2 + score3 + score4;*/
+
+	//double avg_score = static_cast<double>(total_score) / num_students;
+	//Note: double(total_score) / num_students != double(total_score / num_students);
 
 	return 0;
 }
