00ef4b2e89fc467dcfddd3ca3d8055853d88ca42
SEUNGHYUN <ysh7733@naver.com>
2023-05-06 19:13:52 +0900

    Study strcat, strcmp again

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -6,11 +6,20 @@ using namespace std;
 int main()
 {
 	char source[] = "Copy this!";
-	char dest[5];
-	strcpy_s(dest, source);
+	char dest[50] = "Copy this!";
+	//strcpy_s(dest, source);
 
-	cout << source << endl;
-	cout << dest << endl;
-	
+	//strcat() 한 문자열 뒤에 어떠한 문자열을 붙여준다.
+	//strcmp() 두 문자열이 동일한지 비교해본다.
+
+	if (strcmp(source, dest))
+	{
+		cout << "Not Same" << endl;
+	}
+	else if (strcmp(source, dest) == 0)
+	{
+		cout << "Same Same" << endl;
+		
+	}
 	return 0;
 }
