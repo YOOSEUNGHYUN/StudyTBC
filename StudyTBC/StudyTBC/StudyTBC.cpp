decc99e637fcf6c94adb19d72f5aa5789246ed66
SEUNGHYUN <ysh7733@naver.com>
2023-05-06 18:43:50 +0900

    Study C style array char and string

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,51 +2,23 @@
 
 using namespace std;
 
-void printArray(const int array[], const int length)
-{
-	for (int index = 0; index < length; ++index)
-		cout << array[index] << " ";
-	cout << endl;
-}
-
 int main()
 {
-	/*					value	index
-	3 5 2 1 4		     1	      3
-	1 5 2 3 4			 2        2
-	1 2 5 3 4			 3		  3
-	1 2 3 5 4			 4		  4
-	1 2 3 4 5
-	*/
-	
-	const int length = 5;
+	char myString[255];
 
-	int array[length] = { 3, 5, 2, 1, 4 };
-		
-	printArray(array, length);
+	//cin >> myString;
+	cin.getline(myString, 255);
 
-	for (int startIndex = 0; startIndex < length - 1; ++startIndex)
+	//cout << myString << endl;
+
+	int ix = 0;
+	while (true)
 	{
-		int biggestIndex = startIndex;
+		if (myString[ix] == '\0') break;
 
-		for (int currentIndex = startIndex + 1; currentIndex < length; ++currentIndex)
-		{
-			if (array[biggestIndex] < array[currentIndex])
-			{
-				biggestIndex = currentIndex;
-			}			
-		}
-
-		// swap two values in the array
-		// std::swap(array[smallestIndex], array[startIndex]);
-		{
-			int temp = array[biggestIndex];
-			array[biggestIndex] = array[startIndex];
-			array[startIndex] = temp;
-		}
-
-		printArray(array, length);
+		cout << myString[ix] << " " << (int)myString[ix] << endl;
+		++ix;
 	}
-
+	
 	return 0;
 }
