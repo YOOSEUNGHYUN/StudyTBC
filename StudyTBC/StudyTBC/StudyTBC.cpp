f8337f1a7d2556f5b82ab845fb9c755d76f8a306
SEUNGHYUN <ysh7733@naver.com>
2023-05-07 15:48:31 +0900

    Study array calculation 3

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,16 +2,25 @@
 
 using namespace std;
 
+//while, break, 포인터와 증감연산자 ++ptr을 사용하여 문자열을 '\0'은 나오지 않게 출력해봐라 
 int main()
 {
-	int array[] = { 9, 7, 5, 3, 1 };
+	char name[] = "Hello Hyoni";
+	const int n_name = sizeof(name) / sizeof(char);
+	char *ptr = name;
 
-	int *ptr = array;
-	for ( int i = 0; i < 5 ; ++i)
-	{ 
-		//cout << array[i] << " " << (uintptr_t)&array[i] << endl;
-		cout << *(ptr + i) << " " << (uintptr_t)(ptr + i) << endl;
+	while(1)
+	{
+		int i = 0;
+		
+		if (*ptr != '\0')
+		{
+			cout << *(ptr);
+			++ptr;
+		}
+		else
+			break;
 	}
-
+	
 	return 0;
 }
