faca15495d122a054bfa0538a82007381f90ae7e
SEUNGHYUN <ysh7733@naver.com>
2023-05-08 21:30:14 +0900

    Study new and delete

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,31 +2,25 @@
 
 using namespace std;
 
-const char* getName()
-{
-	return "Hyoni";
-}
-
 int main()
 {
-	//char name[] = "Hello Hyoni";
-	/*const char* name = getName();
-	const char *name2 = getName();
+	//int var;
+	//var = 7;
 
-	cout << (uintptr_t)name << endl;
-	cout << (uintptr_t)name2 << endl;*/
+	int *ptr = new int{ 7 };
 
-	/*int int_arr[5] = { 1, 2, 3, 4, 5 };
-	char char_arr[] = "Hello, World!";
-	const char *name = "Hyoni";*/
+	cout << ptr << endl;
+	cout << *ptr << endl;
+	
+	delete ptr;
+	ptr = nullptr;
 
-	//cout에서 문자열은 특별하게 처리한다.
-	//cout << int_arr << endl;		//	주소
-	//cout << char_arr << endl;		// 문자열
-	//cout << name << endl;			// 문자열
-
-	char c = 'Q';
-	cout << &c << endl;				// 이상한 문자 출력됨
-
+	cout << "After delete" << endl;
+	if (ptr != nullptr)
+	{
+		cout << ptr << endl;
+		cout << *ptr << endl;
+	}
+	
 	return 0;
 }
