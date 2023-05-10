b17c1bf103be11d27e48e7dd7af2cfa424d5a0ff
SEUNGHYUN <ysh7733@naver.com>
2023-05-10 23:23:30 +0900

    Study new and delete again3

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -3,28 +3,13 @@ using namespace std;
 
 int main()
 {
-	//int var;
-	//var = 7;
-	int *ptr = new (std::nothrow)int{ 7 };
-
-	if(ptr)
-	{ 
-		cout << ptr << endl;
-		cout << *ptr << endl;
-	}
-	else
+	//memory leak
+	while (true)
 	{
-		cout << "Could not allocate memory" << endl;
-	}
-
-	delete ptr;
-	ptr = nullptr;
-
-	cout << "After delete" << endl;
-	if(ptr != nullptr)
-	{ 
+		int *ptr = new int;
 		cout << ptr << endl;
- 		cout << *ptr << endl;
+
+		delete ptr;
 	}
 
 	return 0;
