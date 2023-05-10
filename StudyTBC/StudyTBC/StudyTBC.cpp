d9f7077279dc9418a09b0b8264be413655b78358
SEUNGHYUN <ysh7733@naver.com>
2023-05-10 21:43:23 +0900

    Study c style char symbolic constatnt again

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 using namespace std;
 
-struct MyStruct
-{
-	int array[5] = { 9, 7, 5, 3, 1 };
-};
-
-void doSomething(MyStruct *ms)
-{
-	cout << sizeof((*ms).array) << endl;
-}
-
 int main()
 {
-	MyStruct ms;
-	cout << ms.array[0] << endl;
-	cout << sizeof(ms.array) << endl;
+	/*int int_arr[5] = { 1, 2, 3, 4, 5 };
+	char char_arr[] = "Hello, world!";
+	const char *name = "Hello Hyoni";
 
-	doSomething(&ms);
+	cout << int_arr << endl;
+	cout << char_arr << endl;
+	cout << name << endl;*/
+
+	char c = 'Q';
+	cout << *&c << endl;
 
 	return 0;
 }
