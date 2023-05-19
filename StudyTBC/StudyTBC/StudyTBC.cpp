a9409cdce8449317ae0d620d285607df6546b2d2
SEUNGHYUN <ysh7733@naver.com>
2023-05-19 22:04:48 +0900

    Study parameter and argument difference

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,38 +1,18 @@
 #include <iostream>
-//#include <array>
-#include <vector>
 using namespace std;
+
+int foo(int x, int y);
+
+int foo(int x, int y)
+{
+	return x + y;
+}// x and y are destroyed here
 
 int main()
 {
-	//std::array<int, 5> array;
+	int x = 1, y = 2;
 
-	int *my_arr = new int[5];
-
-	std::vector<int> array;
-
-	std::vector<int> array2 = { 1, 2, 3, 4, 5 };
-
-	cout << array2.size() << endl;
-
-	std::vector<int> array3 = { 1, 2, 3, };
-
-	cout << array3.size() << endl;
-
-	std::vector<int> array4 { 1, 2, 3, };
-
-	cout << array4.size() << endl;
-
-	vector<int> arr = { 1 ,2 ,3 ,4 , 5 };
-
-	for (auto& itr : arr)
-		cout << itr << " ";
-	cout << endl;
-
-	cout << arr[1] << endl;
-	cout << arr.at(1) << endl;
-
-	delete[] my_arr;
-
+	foo(6, 7); //6, 7 : arguments (actual parameters)
+	foo(x, y + 1);
 	return 0;
 }
