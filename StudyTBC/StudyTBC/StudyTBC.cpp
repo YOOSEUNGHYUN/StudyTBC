8b02301b0b639d39393d019446f9cb43f4ed2cac
SEUNGHYUN <ysh7733@naver.com>
2023-05-22 20:19:39 +0900

    Study call by reference3

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
-#include <cmath> // sin(), cos()
-
 using namespace std;
 
-// 보통 입력은 앞에 두고 출력으로 나갈것들은 뒤에 둔다.
-void getSinCos(const double &degrees, double& sin_out, double& cos_out)
-{
-	static const double pi = 3.141592 / 180.0;
-	const double radians = degrees * pi;
+typedef int* pint;
 
-	sin_out = std::sin(radians);
-	cos_out = std::cos(radians);
+void foo(pint &ptr) //((int*)&ptr)
+{
+	cout << ptr << " " << &ptr << endl;
 }
 
 int main()
 {
-	double sin(0.0);
-	double cos(0.0);
+	int x = 5;
+	//int *ptr = &x;
+	pint ptr = &x;
 
-	getSinCos(30.0, sin, cos);
-
-	cout << sin << " " << cos << endl;
+	cout << ptr << " " << &ptr << endl;
+	foo(ptr);
 
 	return 0;
 }
