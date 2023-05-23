4347f4e2d406683500ce4e4d9f0656cc8c8c5c5e
SEUNGHYUN <ysh7733@naver.com>
2023-05-23 22:25:46 +0900

    Study call by reference 5

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -3,28 +3,21 @@ using namespace std;
 
 typedef int* pint;
 
-void foo(double degrees, double *sin_out, double *cos_out)
+void foo(const int * const ptr, int *arr, int length)
 {
-	*sin_out = 1.0;
-	*cos_out = 2.0;
+	for (int i = 0; i < length; ++i)
+		cout << arr[i] << endl;
+
+	arr[0] = 1.0;
+
+	int x = 1;
+
+	//ptr = &x;
 }
 
 
 int main()
 {
-	int value = 5;
-
-	cout << value << " " << &value << endl;
-
-	int *ptr = &value;
-
-	cout << &ptr << endl;
-
-	double degrees = 30;
-	double sin, cos;
-
-	foo(degrees, &sin, &cos);
-	cout << sin << " " << cos << endl;
 	
 	return 0;
 }
