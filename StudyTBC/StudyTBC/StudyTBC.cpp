3714dc55f92e54526ed8c196787916706fb3c36e
SEUNGHYUN <ysh7733@naver.com>
2023-05-30 12:26:21 +0900

    Study recursive function call fibonacci

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 using namespace std;
 //Recursive Function Call
+// 0 1 2 3 5 8 13 21 ... fibonacci
 
-int sumTo(int sumto)
+int fibonacci(int fibo)
 {
-	if (sumto <= 0)
+	if (fibo <= 0)
 		return 0;
-	else if (sumto <= 1)
-		return 1;
+	else if (fibo == 1) return 1;
 	else
 	{
-		const int sum_minus_one = sumTo(sumto - 1);
-		return sum_minus_one + sumto;
+		return fibonacci(fibo - 2) + fibonacci(fibo - 1);
 	}
-		
 }
 
+void printFibonacci(int (*fibonacci)(int), int n)
+{
+	for (int k = 0; k < n; k++)
+	{
+		cout << fibonacci(k) << " ";
+	}
+	cout << endl;
+}
 int main()
 {
-	cout << sumTo(10) << endl;
+	printFibonacci(fibonacci, 10);
 
 	return 0;
 }
