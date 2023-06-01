f60221ab430faac4f9d9d4a6167ed15005786c39
SEUNGHYUN <ysh7733@naver.com>
2023-06-01 13:20:23 +0900

    Study assert 1

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,36 +1,23 @@
 #include <iostream>
-#include <string>
+#include <cassert> //assert.h
+#include <array>
 using namespace std;
-//Defensive Programming
+//assert
+
+void printValue(const std::array<int, 5> &my_array, const int& ix)
+{
+	assert(ix >= 0);
+	assert(ix <= my_array.size() - 1);
+
+	std::cout << my_array[ix] << std::endl;
+}
 
 int main()
 {
-	// semantic errors
-	//int x;
-	//cin >> x;
+	std::array<int, 5> my_array{ 1, 2, 3, 4, 5 };
 
-	//if (x > 5)
-	//	cout << "x is greater than 5" << endl;
+	printValue(my_array, 100);
 
-	// violated assumption
-
-	string hello = "Hello, my name is Hyoni";
-
-	cout << "Input from 0 to " << hello.size() - 1 << endl;
-
-	while (true)
-	{
-		int ix;
-		cin >> ix;
-
-		if (ix >= 0 && ix <= hello.size() - 1)
-		{
-			cout << hello[ix] << endl;
-			break;
-		}
-		else
-			cout << "Please try again" << endl;
-	}
 
 	return 0;
 }
