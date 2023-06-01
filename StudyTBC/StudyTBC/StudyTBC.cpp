e8ecaa7f8592838fe4e2246660c570990d591f8f
SEUNGHYUN <ysh7733@naver.com>
2023-06-01 13:23:31 +0900

    Study assert 2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -2,21 +2,14 @@
 #include <cassert> //assert.h
 #include <array>
 using namespace std;
-//assert
-
-void printValue(const std::array<int, 5> &my_array, const int& ix)
-{
-	assert(ix >= 0);
-	assert(ix <= my_array.size() - 1);
-
-	std::cout << my_array[ix] << std::endl;
-}
 
 int main()
 {
-	std::array<int, 5> my_array{ 1, 2, 3, 4, 5 };
+	const int x = 5;
 
-	printValue(my_array, 100);
+	assert(x == 5);
+
+	static_assert(x == 5, "x should be 5");
 
 
 	return 0;
