24a478bcbbf471d02d0302e4f521b09c7b62494f
SEUNGHYUN <ysh7733@naver.com>
2023-06-07 23:04:40 +0900

    Study Member Initializer List

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,32 +1,53 @@
 #include <iostream>
 #include <string>
 #include <vector>
-//Constructors
+//Member Initializer List
 using namespace std;
 
-class Second
+class B
 {
+private:
+	int m_b;
+
 public:
-	Second()
+	B(const int& m_b_in)
+		: m_b(m_b_in)
 	{
-		cout << "Class Second Constructor()" << endl;
+
 	}
 };
 
-class First
+class Something
 {
-	Second sec;
+private:
+	int m_i = 100;
+	double m_d = 100.0;
+	char m_c = 'F';
+	int m_arr[5] = { 100, 200, 300, 400, 500 };
+	B m_b{ 1024 };
 
 public:
-	First()
+	Something()
+		: m_i{ 1 }, m_d{ 3.14 }, m_c{ 'a' }, m_arr{ 1,2,3,4,5 }, m_b(m_i - 1) //이렇게 초기화하면 형변환이 안된다. 좀 더 엄격함
+		// : m_i(1), m_d(3.14), m_c('a')
 	{
-		cout << "Class First Constructor()" << endl;
+		m_i *= 3;
+		m_d *= 3.0;
+		m_c += 3;
+	}
+
+	void print()
+	{
+		cout << m_i << " " << m_d << " " << m_c << endl;
+		for (auto& e : m_arr)
+			cout << e << " ";
+		cout << endl;
 	}
 };
-
 int main()
 {
-	First fir;
+	Something som;
+	som.print();
 
 	return 0;
 }
