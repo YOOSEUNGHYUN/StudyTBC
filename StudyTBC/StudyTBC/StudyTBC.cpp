71f074b4eda37f9df283240bbdf56787d5ab9398
SEUNGHYUN <ysh7733@naver.com>
2023-06-13 08:22:17 +0900

    study static member variation1

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//Class and Const
+//static member variation
 
 class Something
 {
 public:
-	string m_value = "default";
-
-	// const냐 아니냐를 가지고 overloading 할 수 있다.
-	const string& getValue() const 
-	{ 
-		cout << "const version" << endl;
-		return m_value; 
-	}
-
-	string& getValue() 
-	{
-		cout << "non-const version" << endl;
-		return m_value; 
-	}
-
+	//static int s_value = 1; // static member var는 initialize 할 수 없다.
+	static int s_value;
 };
 
+int Something::s_value = 1;		// *** static 멤버 변수는 헤더에 넣지말고 cpp 파일에 넣어야 한다. 
+								// *** 헤더에 두면 에러난다. 중복선언 문제 발생한다.
 
 int main()
 {
-	// non-const reference 면 값을 바꿔 줄 수 있다.
-	Something something;
-	something.getValue()=10;
-	
-	// const reference 는 값을 바꿀 수 없다.
-	const Something something2;
-	something2.getValue(); // = 1000 XXXXX
+	cout << &Something::s_value << " " << Something::s_value << endl;
+	// static이라서 밑에 st1,st2가 없어도 s_value에 접근이 가능하다. (메모리에 정적으로 존재한다는 것)
+
+	Something st1;
+	Something st2;
+
+	st1.s_value = 2;	// static 붙이면 st1의 값만 바꿔도 st2 값도 같이 바뀐다.
+
+	cout << &st1.s_value << " " << st1.s_value << endl;
+	cout << &st2.s_value << " " << st2.s_value << endl;
+
+	Something::s_value = 1024;
+
+	cout << &Something::s_value << " " << Something::s_value << endl;
 
 	return 0;
 }
