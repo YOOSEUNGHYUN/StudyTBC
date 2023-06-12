842b5d108b23163a1f7bba290b96a55a84475a7a
SEUNGHYUN <ysh7733@naver.com>
2023-06-12 11:01:32 +0900

    Study Class and Const2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,49 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
 //Class and Const
 
 class Something
 {
 public:
-	int m_value = 0;
+	string m_value = "default";
 
-	// copy constructor
-	Something(const Something& st_in) //이런 역할을 하는게 숨어있다. 그런데 사용자가 수동으로 만들어줄 수 있다.
-	{
-		m_value = st_in.m_value;
-		cout << "Copy Constructor" << endl;
-	}
-
-	Something()
-	{
-		cout << "Constructor" << endl;
-	}
-
-	void setValue(int value) { m_value = value;}
-
-	int getValue() const // 여기에다가 const를 쳐주면 이 멤버함수가 const다 라고 하는 것. 값을 바꾸지 않는다.
+	// const냐 아니냐를 가지고 overloading 할 수 있다.
+	const string& getValue() const 
 	{ 
+		cout << "const version" << endl;
 		return m_value; 
 	}
+
+	string& getValue() 
+	{
+		cout << "non-const version" << endl;
+		return m_value; 
+	}
+
 };
 
 
-void print(const Something &st)	// 특별한 경우가 아니고선 const를 다 붙여라 11:40
-{
-	cout << &st << endl;
-	cout << st.getValue() << endl;
-}
-
 int main()
 {
-	//const Something something;
-	//// something.setValue(3); 애초에 잘못됨
-
-	//cout << something.getValue() << endl; // 컴파일러는 멤버함수가 const냐 아니냐를 판단한다.
-
+	// non-const reference 면 값을 바꿔 줄 수 있다.
 	Something something;
-	cout << &something << endl;
-	print(something);
+	something.getValue()=10;
+	
+	// const reference 는 값을 바꿀 수 없다.
+	const Something something2;
+	something2.getValue(); // = 1000 XXXXX
 
 	return 0;
 }
