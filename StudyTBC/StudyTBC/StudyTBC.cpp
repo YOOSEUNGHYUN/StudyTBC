05d9c2a2c3f8786b555ad1753b5ae605ca68dd68
SEUNGHYUN <ysh7733@naver.com>
2023-06-16 08:31:39 +0900

    Study anonymous object 1

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 using namespace std;
-//friend function and class
-
-class A;	// forward declaration
-
-class B
-{
-private:
-	int m_value = 2;
-
-public:
-	void doSomething(A& a);
-};
+// anonymous object
 
 class A
 {
-private:
-	int m_value = 1;
+public:
+	int m_value;
 
-	//friend class B;
+	A(const int& input)
+		: m_value(input)
+	{
+		cout << "Constructor" << endl;
+	}
 
-	friend void B::doSomething(A& a);
+	~A()
+	{
+		cout << "Destructor" << endl;
+	}
+
+	void printDouble()
+	{
+		cout << m_value*2 << endl;
+	}
 };
-
-void B::doSomething(A& a)
-{
-	cout << a.m_value << endl;
-}
 
 int main()
 {
-	A a;
-	B b;
-	b.doSomething(a);
+	A a(1);
+	a.printDouble();
+
+	A(1).printDouble();	// r-value처럼 작동한다.
+
+	//A a;
+	//a.print();	 //변수 선언하고 여러번 출력할 경우 Constructor와 Destructor가 한번씩만 호출됨.
+	//a.print();
+	//a.print();
+
+	//A().print(); // 별도로 변수를 만들지 않고 함수 실행 가능.
+	//			 // A() 가 r-value 처럼 작동한다.
+	//			 // 쓰고 나면 재사용 못한다.
+	//A().print(); // 또 쓰면 위 아래 객체가 다르다.
 
 	return 0;
 }
