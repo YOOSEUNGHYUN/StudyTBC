78b63fd053fce7c747f8466770d7a68cfa3288f8
SEUNGHYUN <ysh7733@naver.com>
2023-06-19 23:42:15 +0900

    Study Overloading Arithmetic Operator

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,50 +1,49 @@
 #include <iostream>
-#include <vector> // sorting 하는 시간을 재기 위해서 vector include함.
-#include <algorithm> // sort 알고리즘
-#include <random>
-#include <chrono>	// 시간 재주는 라이브러리
+// Overloading Arithmetic Operator
 using namespace std;
-// measure execution time
-// 실제 시간을 잴 때에는 release 모드로 재야 한다. debug 로 하면 안돼
 
-class Timer
+class Cents
 {
-	using clock_t = std::chrono::high_resolution_clock;
-	using second_t = std::chrono::duration<double, std::ratio<1>>;
-
-	std::chrono::time_point<clock_t> start_time = clock_t::now();
+private:
+	int m_cents;
 
 public:
-	void elapsed()
-	{
-		std::chrono::time_point<clock_t> end_time = clock_t::now();
+	Cents(int cents = 0) { m_cents = cents; } // 생성자
+	int getCents() const { return m_cents; } // 외부 access
+	int& getCents() { return m_cents; }
 
-		cout << std::chrono::duration_cast<second_t>(end_time - start_time).count() << endl;
+	// =, [], (), -> 얘네들은 이런형태(member function)으로만 overloading이 가능하다.
+	Cents operator + (const Cents& c2)
+	{
+		return Cents(this->m_cents + c2.m_cents);
 	}
 };
 
+//Cents operator + (const Cents& c1, const Cents& c2)
+//{
+//	return Cents(c1.getCents() + c2.getCents());
+//}
+
 int main()
 {
-	random_device rnd_device;
-	mt19937 mersenne_engine{ rnd_device() };
+	Cents cents1(6);
+	Cents cents2(8);
 
-	vector<int> vec(1000000);
-	/*for (unsigned int i = 0; i < vec.size(); ++i)
-		vec[i] = i;*/
+	/*Cents sum;
+	add(cents1, cents2, sum);*/
 
-	std::shuffle(begin(vec), end(vec), mersenne_engine);
+	/*cout << add(cents1, cents2).getCents() << endl;*/
 
-	for (auto& e : vec) cout << e << " ";
-	cout << endl;
+	cout << (cents1 + cents2 + Cents(6) + Cents(10) + Cents(100)).getCents() << endl;
 
-	Timer timer;
+	/*int i = 6, j = 8;
+	cout << i + j << endl;*/
 
-	std::sort(begin(vec), end(vec));
+	// 오버로딩 안되는 것들
+	// ?: 조건연산자 :: 범위연산자 sizeof . member selection .* member pointer selection
+	//  operator overloading을 하면 연산자 우선순위는 그대로다.
+	// ^ operator는 연산자 우선순위가 매우 낮다. 얘를 쓰려면 괄호로 쌓아야 한다. 왠만하면 overloading 하지마라\
 
-	timer.elapsed();
-
-	/*for (auto& e : vec) cout << e << " ";
-	cout << endl;*/
 
 	return 0;
 }
