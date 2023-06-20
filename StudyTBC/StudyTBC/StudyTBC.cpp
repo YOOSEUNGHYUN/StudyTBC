0cf1ea56d8aaf0ad00c2112448cf249fc5fe6131
SEUNGHYUN <ysh7733@naver.com>
2023-06-21 00:06:50 +0900

    Study Overloading Input Output Operator2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 // Overloading Input Output Operator
 using namespace std;
 
@@ -16,20 +17,44 @@ public:
 	double getY() { return m_y; }
 	double getZ() { return m_z; }
 
-	void print()
+	/*void print()
 	{
 		cout << m_x << " " << m_y << " " << m_z;
+	}*/
+
+	friend std::ostream& operator << (std::ostream &out, const Point &point)
+	{
+		out << "(" << point.m_x << " " << point.m_y << " " << point.m_z << ")";
+
+		return out;
+	}
+
+	friend std::istream& operator >> (std::istream& in, Point& point)
+	{
+		in >> point.m_x >> point.m_y >> point.m_z;
+
+		return in;
 	}
 };
 
 int main()
 {
-	Point p1(0.0, 0.1, 0.2), p2(3.4, 1.5, 2.0);
+	ofstream of("out.txt");
 
-	p1.print();
+	//Point p1(0.0, 0.1, 0.2), p2(3.4, 1.5, 2.0);
+	Point p1, p2;
+
+	cin >> p1 >> p2;
+
+	/*p1.print();
 	cout << " ";
 	p2.print();
-	cout << endl;
+	cout << endl;*/
+
+	cout << p1 << " " << p2 << endl;
+	of << p1 << " " << p2 << endl;
+
+	of.close();
 
 	return 0;
 }
