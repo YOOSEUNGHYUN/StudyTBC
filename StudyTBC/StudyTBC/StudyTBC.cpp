08b0ee0010a9ce799a016e71edf84c9c8ec450ee
SEUNGHYUN <ysh7733@naver.com>
2023-06-20 23:55:58 +0900

    Study Overloading Input Output Operator1

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,49 +1,35 @@
 #include <iostream>
-// Overloading Arithmetic Operator
+// Overloading Input Output Operator
 using namespace std;
 
-class Cents
+class Point
 {
 private:
-	int m_cents;
+	double m_x, m_y, m_z;
 
 public:
-	Cents(int cents = 0) { m_cents = cents; } // 생성자
-	int getCents() const { return m_cents; } // 외부 access
-	int& getCents() { return m_cents; }
+	Point(double x = 0.0, double y = 0.0, double z = 0.0)
+		: m_x(x), m_y(y), m_z(z)
+	{}
 
-	// =, [], (), -> 얘네들은 이런형태(member function)으로만 overloading이 가능하다.
-	Cents operator + (const Cents& c2)
+	double getX() { return m_x; }
+	double getY() { return m_y; }
+	double getZ() { return m_z; }
+
+	void print()
 	{
-		return Cents(this->m_cents + c2.m_cents);
+		cout << m_x << " " << m_y << " " << m_z;
 	}
 };
 
-//Cents operator + (const Cents& c1, const Cents& c2)
-//{
-//	return Cents(c1.getCents() + c2.getCents());
-//}
-
 int main()
 {
-	Cents cents1(6);
-	Cents cents2(8);
+	Point p1(0.0, 0.1, 0.2), p2(3.4, 1.5, 2.0);
 
-	/*Cents sum;
-	add(cents1, cents2, sum);*/
-
-	/*cout << add(cents1, cents2).getCents() << endl;*/
-
-	cout << (cents1 + cents2 + Cents(6) + Cents(10) + Cents(100)).getCents() << endl;
-
-	/*int i = 6, j = 8;
-	cout << i + j << endl;*/
-
-	// 오버로딩 안되는 것들
-	// ?: 조건연산자 :: 범위연산자 sizeof . member selection .* member pointer selection
-	//  operator overloading을 하면 연산자 우선순위는 그대로다.
-	// ^ operator는 연산자 우선순위가 매우 낮다. 얘를 쓰려면 괄호로 쌓아야 한다. 왠만하면 overloading 하지마라\
-
+	p1.print();
+	cout << " ";
+	p2.print();
+	cout << endl;
 
 	return 0;
 }
