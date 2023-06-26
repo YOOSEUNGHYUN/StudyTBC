baf23d4425e6250ff4e52e9e866937250a4aa3c1
SEUNGHYUN <ysh7733@naver.com>
2023-06-26 19:14:33 +0900

    Study Overloading static_cast typecasts

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,20 +1,60 @@
 #include <iostream>
-#include <cassert>
-// Overloading parenthesis operator and Function object(Functor)
+// Overloading static_cast typecasts
 using namespace std;
 
-class Accumulator
+class Cents
 {
 private:
-	int m_counter = 0;
+	int m_cents;
 
 public:
-	int operator()(int i) { return (m_counter += i); }
+	Cents(int cents = 0)
+	{
+		m_cents = cents;
+	}
+
+	int getCents() { return m_cents; }
+	void setCents(int cents) { m_cents = cents; }
+
+	//	int 형으로 변환을 하는 형변환을 오버로딩 하는 것
+	operator int()
+	{
+		cout << "cast here" << endl;
+		return m_cents;
+	}
 };
+
+class Dollar
+{
+private:
+	int m_dollars = 0;
+
+public:
+	Dollar(const int& input) : m_dollars(input) {}
+
+	operator Cents()
+	{
+		return Cents(m_dollars * 100);
+	}
+};
+
+void printInt(const int& value)
+{
+	cout << value << endl;
+}
 
 int main()
 {
-	Accumulator acc;
-	cout << acc(10) << endl;
-	cout << acc(20) << endl;
+	//Cents cents(7);
+	// overloading 한 typecast 연산자를 사용하고 있다는 것을 확인 할 수 있다.
+	/*int value = (int)cents;
+	value = int(cents);
+	value = static_cast<int>(cents);*/
+
+	Dollar dol(2);
+	Cents cents = dol;
+
+	printInt(cents);
+
+	return 0;
 }
