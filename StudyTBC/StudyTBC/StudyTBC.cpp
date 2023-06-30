2ef7c9296f3546c48a324a3f4f0c0610413dd90f
SEUNGHYUN <ysh7733@naver.com>
2023-06-30 22:47:42 +0900

    Overloading assignment operator, Deep copy vs Shallow copy

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,47 +1,109 @@
 #include <iostream>
 #include <cassert>
-// Converting constructor, explicit, delete
+// Overloading assignment operator, Deep copy vs Shallow copy 
 using namespace std;
 
-class Fraction
+std::string;
+
+class MyString
 {
-private:
-	int m_numerator;
-	int m_denominator;
+//private:
+public:
+	char *m_data = nullptr;
+	int m_length = 0;
 
 public:
-	Fraction(char) = delete;
-
-	explicit Fraction(int num = 0, int den = 1)
-		: m_numerator(num), m_denominator(den)
+	MyString(const char* source = "")
 	{
-		assert(den != 0);
+		assert(source);
+
+		m_length = std::strlen(source) + 1;
+		m_data = new char[m_length];
+
+		for (int i = 0; i < m_length; ++i)
+			m_data[i] = source[i];
+
+		m_data[m_length - 1] = '\0';
 	}
 
-	Fraction(const Fraction& fraction) // copy constructor
-		: m_numerator(fraction.m_numerator), m_denominator(fraction.m_denominator)
+	//MyString(const MyString &source) = delete; 이렇게 막는 방법도 있다.
+
+	MyString(const MyString& source)
 	{
-		cout << "Copy constructor called" << endl;
+		cout << "Copy constructor " << endl;
+
+		m_length = source.m_length;
+
+		if (source.m_data != nullptr)
+		{
+			m_data = new char[m_length];
+
+			for (int i = 0; i < m_length; ++i)
+				m_data[i] = source.m_data[i];
+		}
+		else
+			m_data = nullptr;
 	}
 
-	friend std::ostream& operator << (std::ostream& out, const Fraction& f)
+	MyString& operator = (const MyString& source)
 	{
-		out << f.m_numerator << " / " << f.m_denominator << endl;
-		return out;
+		// shallow copy
+		/*this->m_data = source.m_data;
+		this->m_length = source.m_length;*/
+
+		cout << "Assignment operator " << endl;
+
+		if (this == &source) // precent self-assignment 대입연산자는 자기 자신을 대입할 수 있다.
+			return *this;	 // 이 경우 못하게 막아버리고 return 해서 끝낸다.
+
+		delete[] m_data;
+
+		m_length = source.m_length;
+
+		if (source.m_data != nullptr)
+		{
+			m_data = new char[m_length];
+
+			for (int i = 0; i < m_length; ++i)
+				m_data[i] = source.m_data[i];
+		}
+		else
+			m_data = nullptr;
+
+		return *this;
 	}
+
+	~MyString()
+	{
+		delete[] m_data;
+	}
+
+	char* getString() { return m_data; }
+	int getLength() { return m_length; }
+
 };
-
-void doSomething(Fraction frac)
-{
-	cout << frac << endl;
-}
 
 int main()
 {
-	//Fraction frac2('c'); 위에 delete로 막힘
-	Fraction frac(7);
+	MyString hello("Hello");
 
-	doSomething(frac);
+	MyString str1 = hello;	// 여기에 assignment operator가 있지만 Copy Constructor가 호출된다.
+	//MyString str1(hello); 와 같다.
+
+	MyString str2;
+	str2 = hello;	// 그냥 대입만 할 때에는 assignment operator가 호출된다.
+
+	//cout << (int*)hello.m_data << endl;
+	//cout << hello.getString() << endl;
+
+	//{
+	//	MyString copy = hello;	// 복사 생성자로 호출된다. 대입 연산자 아니다.
+	//	cout << (int*)copy.m_data << endl;
+	//	cout << copy.getString() << endl;
+	//}	// copy는 scope를 벗어나면 사라진다.
+	//	// 여기를 벗어나면서 delete에서 지운다.
+	//
+	//cout << hello.getString() << endl;
 
 	return 0;
 }
