2a379d32b3351e3db0d70cc7d16b25b05dcda540
SEUNGHYUN <ysh7733@naver.com>
2023-07-01 21:12:29 +0900

    Study std::initializer_list

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,109 +1,63 @@
 #include <iostream>
 #include <cassert>
-// Overloading assignment operator, Deep copy vs Shallow copy 
+#include <initializer_list>
+// std::initializer_list
 using namespace std;
 
-std::string;
-
-class MyString
+class IntArray
 {
-//private:
-public:
-	char *m_data = nullptr;
-	int m_length = 0;
+private:
+	unsigned m_length = 0;
+	int *m_data = nullptr;
 
 public:
-	MyString(const char* source = "")
+	IntArray(unsigned length)
+		: m_length(length)
 	{
-		assert(source);
-
-		m_length = std::strlen(source) + 1;
-		m_data = new char[m_length];
-
-		for (int i = 0; i < m_length; ++i)
-			m_data[i] = source[i];
-
-		m_data[m_length - 1] = '\0';
+		m_data = new int[length];
 	}
 
-	//MyString(const MyString &source) = delete; 이렇게 막는 방법도 있다.
-
-	MyString(const MyString& source)
+	IntArray(const std::initializer_list<int>& list) // initializer_list를 iterator를 사용해서 반복
+		: IntArray(list.size())
 	{
-		cout << "Copy constructor " << endl;
-
-		m_length = source.m_length;
-
-		if (source.m_data != nullptr)
+		int count = 0;
+		for (auto& element : list)
 		{
-			m_data = new char[m_length];
-
-			for (int i = 0; i < m_length; ++i)
-				m_data[i] = source.m_data[i];
+			m_data[count] = element;
+			++count;
 		}
-		else
-			m_data = nullptr;
+
+		//for (unsigned count = 0; count < list.size(); ++count)
+		//	m_data[count] = list[count]; //error
+		// initialize list는 []를 지원하지 않는다.
 	}
 
-	MyString& operator = (const MyString& source)
+	~IntArray()
 	{
-		// shallow copy
-		/*this->m_data = source.m_data;
-		this->m_length = source.m_length;*/
-
-		cout << "Assignment operator " << endl;
-
-		if (this == &source) // precent self-assignment 대입연산자는 자기 자신을 대입할 수 있다.
-			return *this;	 // 이 경우 못하게 막아버리고 return 해서 끝낸다.
-
-		delete[] m_data;
-
-		m_length = source.m_length;
-
-		if (source.m_data != nullptr)
-		{
-			m_data = new char[m_length];
-
-			for (int i = 0; i < m_length; ++i)
-				m_data[i] = source.m_data[i];
-		}
-		else
-			m_data = nullptr;
-
-		return *this;
+		delete[] this->m_data;
 	}
 
-	~MyString()
+	//TODO: overload operator =
+
+	friend ostream& operator << (ostream& out, IntArray& arr)
 	{
-		delete[] m_data;
+		for (unsigned i = 0; i < arr.m_length; ++i)
+			out << arr.m_data[i] << " ";
+		out << endl;
+		return out;
 	}
-
-	char* getString() { return m_data; }
-	int getLength() { return m_length; }
 
 };
 
 int main()
 {
-	MyString hello("Hello");
+	int my_arr1[5] = { 1, 2, 3, 4, 5 };
+	int* my_arr2 = new int[5]{ 1, 2, 3, 4, 5 };
 
-	MyString str1 = hello;	// 여기에 assignment operator가 있지만 Copy Constructor가 호출된다.
-	//MyString str1(hello); 와 같다.
+	auto il = { 10, 20 ,30 };
 
-	MyString str2;
-	str2 = hello;	// 그냥 대입만 할 때에는 assignment operator가 호출된다.
-
-	//cout << (int*)hello.m_data << endl;
-	//cout << hello.getString() << endl;
-
-	//{
-	//	MyString copy = hello;	// 복사 생성자로 호출된다. 대입 연산자 아니다.
-	//	cout << (int*)copy.m_data << endl;
-	//	cout << copy.getString() << endl;
-	//}	// copy는 scope를 벗어나면 사라진다.
-	//	// 여기를 벗어나면서 delete에서 지운다.
-	//
-	//cout << hello.getString() << endl;
+	IntArray int_array { 1, 2, 3, 4, 5 };
+	cout << int_array << endl;
 
 	return 0;
 }
