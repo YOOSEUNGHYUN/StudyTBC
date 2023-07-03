fde0b4550bfb9cd494385b21c6d9b8ea57f161d9
SEUNGHYUN <ysh7733@naver.com>
2023-07-03 21:35:45 +0900

    Study Object Relationship-Association

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,19 +1,54 @@
-
-#include "Monster.h"
-//Object Relationship-Composition
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Lecture.h"
+//Object Relationship-Association
 
 int main()
 {
-	Monster mon1("Sanson", Position2D(0, 0));
+	using namespace std;
 
-	cout << mon1 << endl;
+	Student *std1 = new Student("Jack Jack", 0);
+	Student *std2 = new Student("Dash", 1);
+	Student *std3 = new Student("Violet", 2);
 
-	//while (1)	// game loop
+	Teacher *teacher1 = new Teacher("Prof. Hong");
+	Teacher *teacher2 = new Teacher("Prof. Good");
+
+	// Composition Relationship
+	Lecture lec1("Introdunctio to Computer programming");
+	lec1.assignTeacher(teacher1);
+	lec1.registerStudent(std1);
+	lec1.registerStudent(std2);
+	lec1.registerStudent(std3);
+
+	Lecture lec2("Computational Thinking");
+	lec2.assignTeacher(teacher2);
+	lec2.registerStudent(std1);
+
+	//TODO: implement Aggregation Relationship
+
+	//test
 	{
-		// event
-		mon1.moveTo(Position2D(1,1));
-		cout << mon1 << endl;
+		cout << lec1 << endl;
+		cout << lec2 << endl;
+
+		//	event
+		lec2.study();
+
+		cout << lec1 << endl;
+		cout << lec2 << endl;
 	}
+
+	// TODO: class HobbyClub
+
+	// delete memory (if necessary)
+	delete std1;
+	delete std2;
+	delete std3;
+
+	delete teacher1;
+	delete teacher2;
+
 	return 0;
 }
