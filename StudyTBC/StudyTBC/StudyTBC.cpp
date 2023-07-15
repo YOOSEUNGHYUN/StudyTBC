27149137909737825af61130e68bfe2f1132cf7f
SEUNGHYUN <ysh7733@naver.com>
2023-07-15 23:35:04 +0900

    Inherited Functions 상속받은 함수를 오버라이딩 하기

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,34 +1,65 @@
 #include <iostream>
-
-// 유도된 클래스에 새로운 기능 추가하기
+// Inherited Functions 상속받은 함수를 오버라이딩 하기
 
 using namespace std;
 
 class Base
 {
-private:
-	int m_value;
+protected:
+	int m_i;
+
 public:
 	Base(int value)
-		: m_value(value)
+		: m_i(value)
 	{
+	}
 
+	void print()
+	{
+		cout << "I'm Base" << endl;
+	}
+
+	friend std::ostream& operator << (std::ostream& out, const Base& b)
+	{
+		out << "This is base output" << endl;
+		return out;
 	}
 };
 
 class Derived : private Base
 {
+private:
+	double m_d;
+
 public:
 	Derived(int value)
 		: Base(value)
 	{
 	
 	}
+
+	void print()
+	{
+		Base::print();
+		cout << "I'm Derived" << endl;
+	}
+
+	friend std::ostream& operator << (std::ostream& out, const Derived& b)
+	{
+		cout << static_cast<Base>(b);
+		out << "This is Derived output" << endl;
+		return out;
+	}
 };
 
 
 int main()
 {
+	Base base(5);
+	cout << base;
+
+	Derived derived(7);
+	cout << derived;
 
 	return 0;
 }
