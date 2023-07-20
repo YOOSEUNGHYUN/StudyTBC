7f279628e0572ab30b9f08e872447056779e1295
SEUNGHYUN <ysh7733@naver.com>
2023-07-20 23:42:43 +0900

    study override, final, covariant return type

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
-// 가상함수와 다형성
-// Virtual Functions and Polymorphism
+// 공변 반환형
+// override, final, covariant return type
 using namespace std;
 
 class A
 {
 public:
-	virtual void print() { cout << "A" << endl; }
+	void print() { cout << "A" << endl; }
+	virtual A* getThis() { 
+		cout << "A::getThis()" << endl; 
+		return this; }
 };
 
 class B : public A
 {
 public:
-	virtual void print() { cout << "B" << endl; }
+	void print() { cout << "B" << endl; } // final 하면 오버라이드 못한다.
+	virtual B* getThis() {
+		cout << "B::getThis()" << endl;
+		return this;
+	}
 };
 
 class C : public B
@@ -21,21 +28,19 @@ public:
 	virtual void print() { cout << "C" << endl; }
 };
 
-class D : public C
-{
-public:
-	virtual void print() { cout << "D" << endl; }
-};
 
 int main()
 {
 	A a;
 	B b;
-	C c;
-	D d;
+	//C c;
 	
-	A& ref = c;
-	ref.print();
+	A& ref = b;
+	b.getThis()->print();
+	ref.getThis()->print();
+
+	cout << typeid(b.getThis()).name() << endl;
+	cout << typeid(ref.getThis()).name() << endl;
 
 	return 0;
 }
