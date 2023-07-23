968fde6e14a11acfee976f5f62ef16e7422bdab4
SEUNGHYUN <ysh7733@naver.com>
2023-07-23 20:50:39 +0900

    Study Virtual Tables

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,53 +1,32 @@
 #include <iostream>
-// 동적 바인딩과 정적 바인딩
+// Virtual Tables
+// 가상 (함수) 표
 
 using namespace std;
 
-int add(int x, int y)
+class Base
 {
-	return x + y;
-}
+public:
+	//FunctionPointer *_vptr;
+	virtual void fun1() {};
+	virtual void fun2() {};
+};
 
-int subtract(int x, int y)
+class Der : public Base
 {
-	return x - y;
-}
-
-int multiply(int x, int y)
-{
-	return x * y;
-}
+public:
+	//FunctionPointer *_vptr;
+	virtual void fun1() {};
+	virtual void fun3() {};
+};
 
 int main()
 {
-	int x, y;
-	cin >> x >> y;
+	Base my_base;
+	Der my_derived;
 
-	int op;
-	cout << "0 : add, 1 : subtract, 2 : multiply" << endl;
-	cin >> op;
-
-	//static binding (early binding)
-	//int result;
-	//switch (op)
-	//{
-	//case 0: result = add(x, y); break;
-	//case 1: result = subtract(x, y); break;
-	//case 2: result = multiply(x, y); break;
-	//}
-
-	//cout << result << endl;
-
-	// Dynamic binding (late binding)
-	int(*func_ptr)(int, int) = nullptr;
-	switch (op)
-	{
-	case 0: func_ptr = add; break;
-	case 1: func_ptr = subtract; break;
-	case 2: func_ptr = multiply; break;
-	}
-
-	cout << func_ptr(x, y) << endl;
+	cout << sizeof(Base) << endl;
+	cout << sizeof(Der) << endl;
 
 	return 0;
 }
