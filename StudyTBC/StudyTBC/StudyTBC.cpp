bba8ad84df240617785d0a1908134ea82e4e2161
SEUNGHYUN <ysh7733@naver.com>
2023-07-31 22:50:20 +0900

    study class template

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,42 +1,18 @@
-#include <iostream>
-#include "Cents.h"
-// 함수 템플릿 templates
+#include "MyArray.h"
+//#include "MyArray.cpp" 이렇게 하면 안돼
+
+// 클래스 템플릿 templates
 using namespace std;
-
-template<typename T>	// typename 대신에 class 적을 수 있음
-T getMax(T x, T y)
-{
-	return (x > y) ? x : y;
-}
-
-//int getMax(int x, int y)
-//{
-//	return (x > y) ? x : y;
-//}
-//
-//double getMax(double x, double y)
-//{
-//	return (x > y) ? x : y;
-//}
-//
-//float getMax(float x, float y)
-//{
-//	return (x > y) ? x : y;
-//}
-//
-//char getMax(char x, char y)
-//{
-//	return (x > y) ? x : y;
-//}
 
 int main()
 {
-	std::cout << getMax(1, 2 ) << std::endl;
-	std::cout << getMax(3.14, 1.592) << std::endl;
-	std::cout << getMax(1.0f, 3.4f) << std::endl;
-	std::cout << getMax('a', 'c') << std::endl;
+	MyArray<char> my_array(10);
 
-	std::cout << getMax(Cents(5), Cents(9)) << std::endl;
+	for (int i = 0; i < my_array.getLength(); ++i)
+		my_array[i] = i + 65;
+
+	my_array.print();
 
 	return 0;
 }
+
