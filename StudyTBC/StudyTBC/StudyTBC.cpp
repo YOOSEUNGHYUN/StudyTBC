781affe1124a9c29362c94dc3363a5def774a606
SEUNGHYUN <ysh7733@naver.com>
2023-08-06 19:05:06 +0900

    예외 처리의 기본 Exception Handling 2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -5,40 +5,25 @@
 // 예외 처리의 기본 Exception Handling
 using namespace std;
 
-int findFirstChar(const char* string, char ch)
-{
-	for (std::size_t index = 0; index < strlen(string); ++index)
-		if (string[index] == ch)
-			return index;
-
-	return -1; // no match
-}
-
-double divide(int x, int y, bool& success)
-{
-	if (y == 0)
-	{
-		success = false;
-		return 0.0;
-	}
-
-	success = false;
-	return static_cast<double>(x) / y;
-}
 
 int main()
 {
-	bool success;
-	double result = divide(5, 3, success);
+	//try, catch, throw
+	double x;
+	cin >> x;
 
-	if (!success)
-		std::cerr << "An error occurred" << std::endl;
-	else
-		std::cout << "Result is " << result << std::endl;
+	try
+	{
+		if (x < 0.0) throw std::string("Negative input");
 
-	std::ifstream input_file("temp.txt");
-	if (!input_file)
-		std::cerr << "Cannot open file" << std::endl;
+		cout << std::sqrt(x) << endl;
+
+	}
+	catch (std::string error_message)
+	{
+		//	do something to respond
+		cout << error_message << endl;
+	}
 
 	return 0;
 }
