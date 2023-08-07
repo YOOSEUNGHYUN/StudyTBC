e3c681d7fe193f1f6dfc608fa0c6a93580b9fff0
SEUNGHYUN <ysh7733@naver.com>
2023-08-07 17:48:20 +0900

    예외 처리와 스택 되감기 Stack Unwinding

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,35 +1,84 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-// 예외 처리의 기본 Exception Handling
+// 예외 처리와 스택 되감기 Stack Unwinding
 using namespace std;
+
+// void last() throw(int) exception specifier
+// void last() throw(...) exception specifier
+
+void last()	throw(...)
+{
+	cout << "last " << endl;
+	cout << "Throws exception" << endl;
+
+	throw 'a';
+
+	cout << "End last " << endl;
+}
+
+void third()
+{
+	cout << "Third" << endl;
+
+	last();
+
+	cout << "End third" << endl;
+}
+
+void second()
+{
+	cout << "Second" << endl;
+
+	try
+	{
+		third();
+	}
+	catch (double)
+	{
+		cerr << "Second caught double exception" << endl;
+	}
+
+	cout << "End second " << endl;
+}
+
+void first()
+{
+	cout << "first" << endl;
+
+	try
+	{
+		second();
+	}
+	catch (int)
+	{
+		cerr << "first caught int exception" << endl;
+	}
+
+	cout << "End first " << endl;
+}
 
 int main()
 {
-	//try, catch, throw
-	
+	cout << "Start" << endl;
+
 	try
 	{
-		//	something happend
-		throw std::string("My error message");
+		first();
 	}
-	catch (int x)
+	catch (int)
 	{
-		cout << "Catch integer" << x << endl;
+		//cerr,cout,clog 로그를 콘솔에 출력하는 채널이라고 생각하면 됨. 
+		cerr << "main caught int exception" << endl;
 	}
-	catch (double x)
+	//	uncaught exceptions
+
+	catch (...) // catch-all handlers
 	{
-		cout << "Catch double " << x << endl;
-	}
-	catch (const char * error_message)
-	{
-		cout << "Char * " << error_message << endl;
-	}
-	catch (std::string error_message)
-	{
-		
-		cout << error_message << endl;
+		cerr << "main caught ellipses exception" << endl;
 	}
 
+	cout << "End main" << endl;
+	
 	return 0;
 }
