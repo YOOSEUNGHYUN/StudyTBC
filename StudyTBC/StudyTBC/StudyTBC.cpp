87f737a00c8a647da4507e69a021a4de19657221
SEUNGHYUN <ysh7733@naver.com>
2023-08-11 13:20:39 +0900

    함수 try

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,40 +1,58 @@
 #include <iostream>
 #include <exception>
 #include <string>
-// std::exception 소개
+// 함수 try
 using namespace std;
 
-class CustomException : public std::exception
+class A
 {
+private:
+	int m_x;
 public:
-	const char* what() const noexcept override
+	A(int x) : m_x(x)
 	{
-		return "Custom exception";
+		if (x <= 0)
+			throw 1;
 	}
 };
 
+class B : public A
+{
+public:
+	B(int x)
+		: A(x)
+	{
+	}
+
+	B(int x) try : A(x)
+	{
+		//	do initialization
+	}
+	catch (...)
+	{
+		cout << "Catch in B constructor " << endl;
+		//throw; 없지만 있는 것 처럼 작동한다.
+	}
+};
+void doSomething()
+//try
+//{
+//	throw - 1;
+//}
+//catch (...)
+//{
+//	cout << "Catch in doSomething()" << endl;
+//}
 
 int main()
 {
 	try
 	{
-		/*std::string s;
-		s.resize(-1);*/
-
-		//throw std::runtime_error("Bad thing happend");
-		throw CustomException();
+		//doSomething();
+		B b(0);
 	}
-	/*catch (std::length_error& exception)
+	catch (...)
 	{
-		std::cerr << "Length_Error" << std::endl;
-		std::cerr << exception.what() << std::endl;
-	}*/
-	catch (std::exception& exception)
-	{
-		std::cout << typeid(exception).name() << std::endl;
-		std::cerr << exception.what() << std::endl;
+		cout << "Catch in main()" << endl;
 	}
-
-	return 0;
-
 }
