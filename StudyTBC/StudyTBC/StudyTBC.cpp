cbc3c7650e39997964518c66a153982742e7ad7a
SEUNGHYUN <ysh7733@naver.com>
2023-08-18 23:53:41 +0900

    std::unique_ptr

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,80 +1,91 @@
 #include <iostream>
-#include <utility>	// std::move
-#include "AutoPtr.h"
+#include <memory>
 #include "Resource.h"
+
+//	std::unique_ptr
 
 using namespace std;
 
-template<class T>
-void MySwap(T& a, T& b)
+auto doSomething()
 {
-	/*T tmp = a;
-	a = b;
-	b = tmp;*/
+	/*auto res1 = std::make_unique<Resource>(5);
+	return res1;
 
-	T tmp{ std::move(a) };
-	a = std::move(b);
-	b = std::move(tmp);
+	return std::unique_ptr<Resource>(new Resource(5));*/
+
+	return std::make_unique<Resource>(5);
+}
+
+void doSomething2(std::unique_ptr<Resource> & res)
+//void doSomething2(Resource* res)
+{
+	res->setAll(10);
 }
 
 int main()
 {
-//{
-//	AutoPtr<Resource> res1(new Resource(10000000));
-//
-//	cout << res1.m_ptr << endl;
-//
-//	AutoPtr<Resource> res2 = std::move(res1);
-//
-//	cout << res1.m_ptr << endl;
-//	cout << res2.m_ptr << endl;
-//
-//	return 0;
-//}
+	{
+		//Resource* res = new Resource(10000000);
 
-//{
-//	AutoPtr<Resource> res1(new Resource(3));
-//	res1->setAll(3);
-//
-//	AutoPtr<Resource> res2(new Resource(5));
-//	res2->setAll(5);
-//
-//	res1->print();
-//	res2->print();
-//
-//	MySwap(res1, res2);
-//
-//	res1->print();
-//	res2->print();
-//}
+		//std::unique_ptr<Resource> res(new Resource(10000000));
 
-//{
-//	vector<string> v;
-//	string str = "Hello";
-//
-//	v.push_back(str);
-//
-//	cout << str << endl;
-//	cout << v[0] << endl;
-//
-//	v.push_back(std::move(str));
-//
-//	cout << str << endl;
-//	cout << v[0] << " " << v[1] << endl;
-//}
+		//	early return or throw
 
-{
-	std::string x{ "abc" };
-	std::string y{ "de" };
+		//delete res;
+	}
 
-	std::cout << "x: " << x << std::endl;
-	std::cout << "y: " << y << std::endl;
+	//{
+	//	std::unique_ptr<int> upi(new int);
 
-	MySwap(x, y);
+	//	/*auto* ptr = new Resource(5);
+	//	std::unique_ptr<Resource> res1(ptr);*/
+	//	//std::unique_ptr<Resource> res1(new Resource(5));
+	//	auto res1 = std::make_unique<Resource>(5);
+	//	//auto res1 = doSomething();
 
-	std::cout << "x: " << x << std::endl;
-	std::cout << "y: " << y << std::endl;
-}
+	//	res1->setAll(5);
+	//	res1->print();
 
+	//	std::unique_ptr<Resource> res2;
+
+	//	std::cout << std::boolalpha;
+	//	std::cout << static_cast<bool>(res1) << std::endl;
+	//	std::cout << static_cast<bool>(res2) << std::endl;
+
+	//	//res2 = res1; unique_ptr 은 복사가 안된다.
+	//	res2 = std::move(res1);
+
+	//	std::cout << std::boolalpha;
+	//	std::cout << static_cast<bool>(res1) << std::endl;
+	//	std::cout << static_cast<bool>(res2) << std::endl;
+
+	//	if (res1 != nullptr) res1->print();
+	//	if (res2 != nullptr) res2->print(); //	(*res2).print();
+	//}
+
+	//{
+	//	auto res1 = std::make_unique<Resource>(5);
+	//	res1->setAll(1);
+	//	res1->print();
+
+	//	//doSomething2(res1);
+	//	doSomething2(res1.get());
+
+	//	std::cout << std::boolalpha;
+	//	std::cout << static_cast<bool>(res1) << std::endl;
+	//	res1->print();
+	//}
+
+	//	이렇게 하면 안돼
+	/*{
+		Resource* res = new Resource;
+		std::unique_ptr<Resource> res1(res);
+		std::unique_ptr<Resource> res2(res);
+
+		delete res;
+	}*/
+		
 	return 0;
+		
+	
 }
