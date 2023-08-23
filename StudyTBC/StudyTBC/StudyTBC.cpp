5975e221edf56f713c87ca31ef994dc67db31cb8
SEUNGHYUN <ysh7733@naver.com>
2023-08-23 10:39:51 +0900

    std::string 과 std::wstring

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,45 +1,56 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstddef>
+#include <string>
+#include <locale>
 
-//	  STL ¾Ë°í¸®Áò ¼Ò°³ Algorithms
+//	  std::string °ú std::wstring
 using namespace std;
 
 int main()
 {
-	vector<int> container;
-	for (int i = 0; i < 10; ++i)
-		container.push_back(i);
+	//	c-style string example
+	//{
+	//	char* strHello = new char[7];
+	//	strcpy_s(strHello, sizeof(char) * 7, "hello!");
+	//	std::cout << strHello << std::endl;
+	//}
 
-	//auto itr = li.begin();
-	//	list<int>::const_iterator itr;
-	auto itr = min_element(container.begin(), container.end());
-	cout << *itr << endl;
+	//basic_string<>, string, wstring
+	{
+		std::string string;
+		std::wstring wstring;
 
-	itr = max_element(container.begin(), container.end());
-	cout << *itr << endl;
+		wchar_t wc;	// µ¥ÀÌÅÍ »çÀÌÁî Å« ¹®ÀÚ wide-character/unicode ±¹Á¦¾î Ç¥ÇöÇÒ ¶§ ¸¹ÀÌ »ç¿ë
+	}
+	
+	//	wstring example
+	{
+		//	Stackoverflow.com
+		//	C++ Code to cout international characters using Visual Studio 2015 ·Î °Ë»ö
+		// http://stackoverflow.com/questions/33404685/c-code-to-cout-international-characters-using-visual-strudio-2015
 
-	cout << endl;
+		const std::wstring texts[] =
+		{
+			L"¾È³çÇÏ¼¼¿ä?", //Korean
+			L"??", //Spanish
+			L"for?t int?r?t",  //French
+			L"Ges?©¬",  //German
+			L"ö¢á¼÷îáÌìíÐôÑç",  //Chinesse
+			L"ìíÜâìÑªÎ«Ó«Ã«È",  //Japanese
+			L"¬ß¬Ö¬Þ¬ß¬à¬Ô¬à ¬â¬å¬ã¬ã¬Ü¬Ú¬Û",  //Russian
+			L"?¥í¥á ¥ê¥ï¥ì¥ì?¥ó¥é ¥ó¥ç? ¥å¥ë¥ë¥ç¥í¥é¥ê??", //Greek
+			L"?????? ?? ??? ????",  //Punjabi
+			L"??? ?? ????? ",  //Persian
+			L"???? ? ??? ???? ?????", //Telugu
+			L"¬¯¬à ¬Ü¬Ñ¬Ü¬Ó¬à, ¬á¬à ¬Õ¬ñ¬Ó¬à¬Ý¬Ú¬ä¬Ö, ¬Ö ¬ä¬à¬Ó¬Ñ?",  //Bulgarian
+		};
 
-	itr = find(container.begin(), container.end(), 3);
-	container.insert(itr, 128);
+		std::locale::global(std::locale(""));
+		std::wcout.imbue(std::locale());
 
-	for (auto& e : container) cout << e << " ";
-	cout << endl;
-
-	sort(container.begin(), container.end());
-	//li.sort();
-	//container.sort();
-
-	for (auto& e : container) cout << e << " ";
-	cout << endl;
-
-	reverse(container.begin(), container.end());
-	//li.reverse();
-	//container.reverse();
-
-	for (auto& e : container) cout << e << " ";
-	cout << endl;
+		for (size_t i = 0; i < 11; ++i)
+			std::wcout << texts[i] << std::endl;
+	}
 
 	return 0;
 }
