b5f2a0141b3587d4a1c0f1ebc0ca1cff2235341d
SEUNGHYUN <ysh7733@naver.com>
2023-08-24 17:30:44 +0900

    std::string의 길이와 용량 2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 int main()
 {
-	string my_str("012345678");
+	string my_str("01234567");
+	my_str.reserve(1000);		//	최소한 이정도는 잡아달라는 의미.
 
-	cout << my_str.size() << endl;	// C style은 뒤에 null char이 추가되는데 C++은 아님.
-									// string은 class 이다. 
-									// 내부적으로 포인터말고 문자열 char *말고 길이를 직접 가지고 있다.
-									// 문자가 끝났는지 안끝났는지 null char을 찾을 필요가 없다.
+	cout << std::boolalpha;
+	cout << my_str.empty() << endl;
+	cout << my_str.length() << endl;
+	cout << my_str.size() << endl;
+	cout << my_str.capacity() << endl;
+	cout << my_str.max_size() << endl;
 
 	return 0;
 }
