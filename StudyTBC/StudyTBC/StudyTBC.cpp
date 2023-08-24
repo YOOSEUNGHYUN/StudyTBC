da4f66a39c6d6ea51d6a7a35e56c43344c7a1724
SEUNGHYUN <ysh7733@naver.com>
2023-08-24 17:34:21 +0900

    std::string 문자 접근하기와 배열로의 변환 1

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-// std::string의 길이와 용량
+// std::string 문자 접근하기와 배열로의 변환
 using namespace std;
 
 int main()
 {
-	string my_str("01234567");
-	my_str.reserve(1000);		//	최소한 이정도는 잡아달라는 의미.
+	string my_str("abcdefg");
+	
+	cout << my_str[0] << endl;
+	cout << my_str[3] << endl;
 
-	cout << std::boolalpha;
-	cout << my_str.empty() << endl;
-	cout << my_str.length() << endl;
-	cout << my_str.size() << endl;
-	cout << my_str.capacity() << endl;
-	cout << my_str.max_size() << endl;
+	my_str[3] = 'Z';
+
+	cout << my_str << endl;
 
 	return 0;
 }
