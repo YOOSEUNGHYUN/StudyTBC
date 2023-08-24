5e75061db12df48a764fd5a1527638e7f759ca48
SEUNGHYUN <ysh7733@naver.com>
2023-08-24 17:45:12 +0900

    std::string 문자 접근하기와 배열로의 변환 4

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -7,16 +7,15 @@ using namespace std;
 
 int main()
 {
-	string my_str("abcdefg"); 
-	
-	cout << my_str.c_str() << endl;		// const char
+	string my_str("abcdefg");
 
-	const char* arr = my_str.data();	// c_str과 같은 함수 data() 가 있다.
-										// 마지막에 null char 붙는다.
+	char buf[20];
 
-	cout << (int)arr[6] << endl;
-	cout << (int)arr[7] << endl;		// string은 null char가 저장되지 않는다.
-										// 그러나 c_str로 가져올 때는 뒤에 null char 붙어서 가져온다.
+	my_str.copy(buf, 5, 1);
+
+	buf[5] = '\0';
+
+	cout << buf << endl;
 
 	return 0;
 }
