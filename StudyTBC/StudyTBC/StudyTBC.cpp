c0cb620b521bdfc2c072fec901ca50be7b1e628e
SEUNGHYUN <ysh7733@naver.com>
2023-08-24 16:33:46 +0900

    std::string의 여러가지 생성자들과 형변환 2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>	//	string stream
 
 //	  std::string의 여러가지 생성자들과 형변환
 using namespace std;
 
+template <typename T>
+std::string ToString(T x)
+{
+	std::ostringstream osstream;
+	ossstream << x;
+	return osstream.str();
+}
+
+template <typename T>
+bool FromString(const std::string& str, T& x)
+{
+	std::istringstream isstream(str);	// input string stream을 초기화 할 때 string을 넣어서 초기화
+	return (isstream >> x) ? true : false;
+}
+
 int main()
 {
-	//std::string my_string("my string");
-	//const char* my_string = "my string";
+	std::string my_str(ToString(3.141592));
 
-	std::vector<char> vec;
-	for (auto e : "Today is a good day.")
-		vec.push_back(e);
+	double d;
 
-	std::string second_string(vec.begin(), std::find(vec.begin(), vec.end(), 'y'));
+	if (FromString(my_str, d))
+		std::cout << d << std::endl;
+	else
+		std::cout << "Cannot convert string to double" << std::endl;
 
-	std::cout << second_string << std::endl;
+	std::cout << my_str << std::endl;
+	
 
 	return 0;
 }
