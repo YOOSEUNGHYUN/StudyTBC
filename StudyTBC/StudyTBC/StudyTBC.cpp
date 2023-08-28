9c3b0baf1b5b2e6f17ec2c7ad11b34fe70049acc
SEUNGHYUN <ysh7733@naver.com>
2023-08-28 10:51:11 +0900

    ostream으로 출력하기2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -6,24 +6,19 @@ using namespace std;
 
 int main()
 {
-	//cout.setf(std::ios::showpos);	// plus 기호 출력
-	//cout << 108 << endl;
+	//cout << std::boolalpha;	//	bool값이 0과 1이 아닌 true, false로 나온다.
+	//cout << true << " " << false << endl;
 
-	//cout.unsetf(std::ios::showpos);	// plus 기호 사라짐
-	//cout << 109 << endl;
+	//cout << std::defaultfloat;	// 기본
+	//cout << std::scientific << std::uppercase;	// 알파벳 E 대문자로
+	//cout << std::fixed;				//	소수점 뒷 자리 고정
+	cout << std::showpoint;	// 소수점이 나온다.
+	cout << std::setprecision(3) << 123.456 << endl;	
+	cout << std::setprecision(4) << 123.456 << endl;	// 반올림
+	cout << std::setprecision(5) << 123.456 << endl;
+	cout << std::setprecision(6) << 123.456 << endl;
+	cout << std::setprecision(7) << 123.456 << endl;
 
-	//cout.unsetf(std::ios::dec);	//	10진수 모드 끄기
-	//cout.setf(std::ios::hex);	//	16진수 모드 켜기
-
-	//cout.setf(std::ios::hex, std::ios::basefield);	// basefield에 hex 바로 set
-
-	//cout.setf(std::ios::uppercase);	//	16진수 알파벳 대문자로 출력
-	cout << std::hex << std::uppercase;
-	cout << 108 << endl;
-	cout << 109 << endl;
-
-	cout << std::dec;
-	cout << 110 << endl;
 
 	return 0;
 }
