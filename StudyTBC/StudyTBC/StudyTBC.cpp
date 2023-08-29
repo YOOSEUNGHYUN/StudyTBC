406e33d8b274ef85afab013ce8411646f3f45f1e
SEUNGHYUN <ysh7733@naver.com>
2023-08-29 15:42:32 +0900

    stream states and input validation 흐름 상태와 입력 유효성 검증

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,39 +1,84 @@
-#include <iostream>		//	cout 사용
-#include <sstream>		//	string stream 의 약자
-// string stream(버퍼 역할 하는) 문자열 흐름
+#include <iostream>		
+#include <cctype>
+#include <string>
+#include <bitset>
+// stream states and input validation 흐름 상태와 입력 유효성 검증
 using namespace std;
+
+void printCharacterClassification(const int& i)
+{
+	cout << boolalpha;
+	cout << "isalnum " << bool(std::isalnum(i)) << endl;	// <cctpye>에 있음, 알파벳 또는 넘버, return type이 integer다.
+	cout << "isblank " << bool(std::isblank(i)) << endl;
+	cout << "isdigit " << bool(std::isdigit(i)) << endl;	// decimal digit 10진수 숫자냐
+	cout << "islower " << bool(std::islower(i)) << endl;	// 소문자냐
+	cout << "isupper " << bool(std::isupper(i)) << endl;	// 대문자냐
+
+	//www.cplusplus.com/reference/cctype/
+}
+
+void printStates(const std::ios& stream)
+{
+	//	stream은 file과 console에 대해서 일관성있게 구현할때 편함.
+	//	특히 file 입출력을 같이 할 때 
+	//	C 언어에서는 fprintf()를 별도로 사용하는데
+	//	C++ 에서는 console 출력을 위한 코드를 파일 입출력을 위해서 그대로 사용할 수 있다.
+	cout << boolalpha;
+	cout << "good()=" << stream.good() << endl;
+	cout << "eof()=" << stream.eof() << endl;	//	end of file 파일을 다 읽었는지 체크할 때
+	cout << "fail()=" << stream.fail() << endl;	//	실패했을 때 true, 좋은 상태면 false
+	cout << "bad()=" << stream.bad() << endl;
+
+	//www.cpluscpls.com/reference/ios/ios/
+}
+
+void classifyCharacters(const string& str)	
+{
+	//	입력받은 string을 한글자씩 다 뒤져본다.
+
+	for (auto e : str)
+	{
+		cout << e << endl;
+		std::cout << "isdigit " << std::isdigit(e) << endl;
+		std::cout << "isblank " << std::isblank(e) << endl;
+		std::cout << "isalpha " << std::isalpha(e) << endl;
+	}
+}
 
 int main()
 {
-	// 빈칸으로 구분한다.
+	//while (true)
+	//{
+	//	char i;
+	//	cin >> i;		// cin에서는 blank를 자른다.
 
-	stringstream os;
+	//	printStates(cin);
 
-	//int i = 12345;
-	//double d = 67.89;
+	//	//cout << i << endl;
 
-	//os << i << " " << d;
-	os << "12345 67.89";
+	//	//cout << boolalpha;
+	//	//cout << std::bitset<8>(cin.rdstate()) << endl;			// good대신에 readstate 호출
+	//	//cout << std::bitset<8>(std::istream::goodbit) << endl;	//	mask
+	//	//cout << std::bitset<8>(std::istream::failbit) << endl;	//	mask
+	//	//cout << !bool((cin.rdstate() & std::istream::failbit) != 0) << endl;	
+	//	//	readstate로 읽어온 flag로 부터 failbit mask를 이용해서 현재 상태를 추출
+	//	//	logical and operation을 해서 0이 아니라면 fail인 상태 
+	//	//	goodbit 을 호출하려면 앞에 not operator를 붙여줌
+	//	//cin.rdstate() == std::istream::goodbit;	// 이렇게 추출하는 방법도 있음.
 
-	//string str1;
-	//string str2;
+	//	printCharacterClassification(i);
 
-	//os >> str1 >> str2;
+	//	cin.clear();
+	//	cin.ignore(1024, '\n');	// 앞에서 buffer에 입력받은 것들 다 처리 하고 다시 받기위해서 청소
+	//	cout << endl;
+	//}
 
-	//cout << str1 << "|" << str2 << endl;
+	cout << boolalpha;
+	classifyCharacters("1234");
+	classifyCharacters("a 1234");
 
-	/*int i2;
-	double d2;
-
-	os >> i2 >> d2;
-
-	cout << i2 << "|" << d2 << endl;*/
-
-	//	stringstream을 비우려면
-	os.str("");			//	방법 1
-	os.str(string());	//	방법 2
-	os.clear();			//	방법 3	먼저 os.str(""); 하고 써야함. state까지 초기화한다.
-	cout << os.str() << endl;
+	//	regular expression
 
 	return 0;
 }
+
