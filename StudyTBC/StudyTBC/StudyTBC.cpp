f09a1dc4bbe2190a68d145c4b9631981d443f700
SEUNGHYUN <ysh7733@naver.com>
2023-09-01 22:01:48 +0900

    파일의 임의 위치 접근하기

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -5,91 +5,53 @@
 #include <sstream>
 using namespace std;
 
-//	기본적인 파일 입출력
+//	파일의 임의 위치 접근하기
 
 int main()
 {
-	//	writing
-	if (true)
-	{
-		//	out file stream
-		ofstream ofs("my_first_file.dat", ios::app);	//	ios::app, ios::binary
-														//	append 모드도 가능 (파일 존재하면 그 뒤에 이어서 작업)
-		//ofs.open("my_first_file.dat");
+	const string filename = "my_file.txt";
 
-		if (!ofs)
+	//	make a file
+	{
+		ofstream ofs(filename);
+
+		for (char i = 'a'; i <= 'z'; ++i)
+			ofs << i;
+		ofs << endl;
+
+	//	read the file
 		{
-			cerr << "Couldn't open file " << endl;
-			exit(1);
+			ifstream ifs("my_file.txt");
+
+			ifs.seekg(5);//ifs.seekg(5, ios::beg);
+			cout << (char)ifs.get() << endl;
+
+			ifs.seekg(5, ios::cur);
+			cout << (char)ifs.get() << endl;
+
+			//ifs.seekg(-5, ios::end);
+
+			/*ifs.seekg(0, ios::end);
+			cout << ifs.tellg() << endl;*/	//	 현재 위치 알려줌
+
+			/*string str;
+			getline(ifs, str);
+
+			cout << str << endl;*/
 		}
 
-		//	file writing
-		//ofs << "Line 1" << endl;		//	ofs를 이용하면 텍스트로 저장이 된다.
-		//ofs << "Line 2" << endl;
+		{
+			//fstream iofs(filename, ios::in | ios::out);
+			fstream iofs(filename);
 
-		//	binary로 저장할 떄는 이 데이터가 어디까지있는지 알 수 없다. 미리 약속을 해서 알고 있어야 함
-		const unsigned num_data = 10;
-		ofs.write((char*)&num_data, sizeof(num_data));
+			iofs.seekg(5);
+			cout << (char)iofs.get() << endl;	// read
 
-		for (int i = 0; i < num_data; ++i)
-			ofs.write((char*)&i, sizeof(i));
+			iofs.seekg(5);
+			iofs.put('A');	//	write
+		}
 
-		/*stringstream ss;
-		ss << "Line 1" << endl;
-		ss << "Line 2" << endl;
-		string str = ss.str();
-
-		unsigned str_length = str.size();
-		ofs.write((char*)&str_length, sizeof(str_length));
-		ofs.write(str.c_str(), str_length);*/
-
-		//ofs.close();	//	not necessary
+		return 0;
 	}
 
-	//	reading
-	if (true)
-	{
-		//	input file stream
-		ifstream ifs("my_first_file.dat"/*, ios::binary*/);
-
-		if (!ifs)
-		{
-			cerr << "Cannot open file" << endl;
-			exit(1);
-		}
-
-		while (ifs)
-		{
-			std::string str;
-			getline(ifs, str);	// string으로 한줄씩 읽는다. 끝까지 읽으면 while문이 false 됨.
-
-			std::cout << str << endl;
-		}
-
-		//	unsigned ineger가 저장되어 있으니까 읽어들여서 데이터가 몇개 있는지 확인 
-		//unsigned num_data = 0;
-		//ifs.read((char*)&num_data, sizeof(num_data));
-
-		////	실제로 읽어들임
-		//for (unsigned i = 0; i < num_data; ++i)
-		//{
-		//	int num;
-		//	ifs.read((char*)&num, sizeof(num));
-
-		//	std::cout << num << endl;
-		//}
-
-		/*unsigned str_len = 0;
-		ifs.read((char*)&str_len, sizeof(str_len));
-
-		string str;
-		str.resize(str_len);
-		ifs.read(&str[0], str_len);
-
-		cout << str << endl;*/
-
-		//ifs.close();	//	not necessary
-	}
-
-	return 0;
 }
