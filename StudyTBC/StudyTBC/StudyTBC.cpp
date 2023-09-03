509a194fd3ed3957158d5fa296ab2f86384cc337
SEUNGHYUN <ysh7733@naver.com>
2023-09-03 21:29:17 +0900

    함수에서 여러개의 리턴값 반환하기 C++ 17 tuple

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,72 +1,22 @@
 #include <iostream>		
-#include <string>
-#include <vector>
-#include <algorithm>	//	for_each()
-#include <functional>
+#include <tuple>
 using namespace std;
 
-//	람다 함수 lambda function and std::function
+//	함수에서 여러개의 리턴값 반환하기 C++ 17
 
-void goodbye(const string& s)
+auto my_func()
 {
-	cout << "Goodbye " << s << endl;
+	return tuple(123, 456, 789, 10);
 }
-
-class Object
-{
-public:
-	void hello(const string& s)
-	{
-		cout << "Hello " << s << endl;
-	}
-};
-
 int main()
 {
-	//	lambda-introducer
-	//	lambda-parameter-declaration
-	//	lambda-return-type-clause
-	//	compound-statement
-	auto func = [](const int& i) -> void { cout << "Hello, World!" << endl; };
+	auto [a, b, c, d] = my_func();
 
-	func(123);
+	cout << a << " " << b << " " << c << " " << d << " " << endl;
 
-	[](const int& i) -> void { cout << "Hello, World!" << endl; } (1234);
+	//auto result = my_func();
 
-	{
-		string name = "Hyoni";
-		[&]() {std::cout << name << endl; } ();
-	}
+	//cout << get<0>(result) << " " << get<1>(result) <<  " " << get<2>(result) << endl;
 
-	vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-
-	auto func2 = [](int val) {cout << val << endl; };
-	for_each(v.begin(), v.end(), func2);
-	for_each(v.begin(), v.end(), [](int val) {cout << val << endl; });
-
-	cout << []() -> int {return 1; }() << endl;
-
-	std::function<void(int)> func3 = func2;
-	func3(123);
-
-	std::function<void()>func4 = std::bind(func3, 456);
-	func4();
-
-	//	http://en.cppreference.com/w/cpp/utility/functional/placeholders
-	{
-		Object instance;
-		auto f = std::bind(&Object::hello, &instance, std::placeholders::_1);
-
-		f(string("World"));
-
-		auto f2 = std::bind(&goodbye, std::placeholders::_1);
-
-		f2(string("World"));
-	}
-	
 	return 0;
-	
-
 }
