e4d73fcf663a7d09c677bb355aa19f15e426bd60
SEUNGHYUN <ysh7733@naver.com>
2023-09-04 22:34:40 +0900

    std::thread와 멀티쓰레딩 multithreading 의 기초 2

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -3,49 +3,32 @@
 #include <thread>
 #include <chrono>
 #include <vector>
-#include <mutex>
+#include <mutex>	// mutual exclusion (상호배제) 이거 내꺼 나만쓸 수 있어라고 선언
 using namespace std;
 
 //	std::thread와 멀티쓰레딩 multithreading 의 기초
 
+mutex mtx;
+
 int main()
 {
-	/*cout << std::thread::hardware_concurrency() << endl;
-	cout << std::this_thread::get_id() << endl;*/
+	auto work_func = [](const string& name)
+	{
+		for (int i = 0; i < 5; ++i)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-	const int num_pro = std::thread::hardware_concurrency();
+			mtx.lock();
+			cout << name << " " << std::this_thread::get_id() << " is working " << i << endl;
+			mtx.unlock();
+		}
+	};
 
-	cout << std::this_thread::get_id() << endl;
+	std::thread t1 = std::thread(work_func, "Hyoni");
+	std::thread t2 = std::thread(work_func, "Dash");
+	
+	t1.join();
+	t2.join();
 
-	vector<std::thread> my_threads;
-	my_threads.resize(num_pro);
-
-	for (auto& e : my_threads)
-		e = std::thread([]() {
-					cout << std::this_thread::get_id() << endl;
-					while (true) {}});
-	for (auto& e : my_threads)
-		e.join();
-
-
-	//std::thread t1 = std::thread([]() {
-	//	cout << std::this_thread::get_id() << endl; 
-	//	while (true) {}});
-
-	//std::thread t2 = std::thread([]() {
-	//	cout << std::this_thread::get_id() << endl;
-	//	while (true) {}});
-
-	//std::thread t3 = std::thread([]() {
-	//	cout << std::this_thread::get_id() << endl;
-	//	while (true) {}});
-
-	//std::thread t4 = std::thread([]() {
-	//	cout << std::this_thread::get_id() << endl;
-	//	while (true) {}});
-
-	//t1.join();	//	t1이 끝날 때 까지 기다린다.
-	//t2.join();
-	//t3.join();
-	//t4.join();
+	return 0;
 }
