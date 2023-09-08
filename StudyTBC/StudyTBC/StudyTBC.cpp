60c8dd08e4069a409da072e0536b434d03f6a6bc
SEUNGHYUN <ysh7733@naver.com>
2023-09-08 13:17:07 +0900

    Race Condition, std::atomic, std::scoped_lock 레이스 컨디션

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,34 +1,44 @@
 #include <iostream>		
-#include <string>
 #include <thread>
+#include <atomic>	// 쪼갤 수 없다는 의미
+					//	operation이 읽어오고 값을 바꾸고 갖고 가는 세단계를 한번에 해치우도록 묶은 변수
 #include <chrono>
-#include <vector>
 #include <mutex>	// mutual exclusion (상호배제) 이거 내꺼 나만쓸 수 있어라고 선언
 using namespace std;
 
-//	std::thread와 멀티쓰레딩 multithreading 의 기초
+//	Race Condition, std::atomic, std::scoped_lock 레이스 컨디션
 
 mutex mtx;
 
 int main()
 {
-	auto work_func = [](const string& name)
-	{
-		for (int i = 0; i < 5; ++i)
-		{
-			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	int shared_memory(0);
 
-			mtx.lock();
-			cout << name << " " << std::this_thread::get_id() << " is working " << i << endl;
-			mtx.unlock();
+	auto count_func = [&]() {
+		for (int i = 0; i < 1000; ++i)
+		{
+			this_thread::sleep_for(chrono::microseconds(1));
+
+			//mtx.lock();	// lock 걸었으면 반드시 unlock 걸어줘야함.
+
+			//std::lock_guard lock(mtx);
+			std::scoped_lock lock(mtx);
+
+			shared_memory++;	//	++은 atomic<int>의 증감 연산자가 overloading 되있는거. 
+								//	정의가 따로 되어있음
+			//shared_memory.fetch_add(1);	//	그냥 int의 더하기 빼기 보다는 속도가 조금 느리다.
+			//mtx.unlock();
 		}
 	};
 
-	std::thread t1 = std::thread(work_func, "Hyoni");
-	std::thread t2 = std::thread(work_func, "Dash");
-	
+	thread t1 = thread(count_func);
+	thread t2 = thread(count_func);
+
 	t1.join();
 	t2.join();
+
+	cout << "After" << endl;
+	cout << shared_memory << endl;
 
 	return 0;
 }
