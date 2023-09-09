2e18839cc94fd25b13417ead7ef99b8772d1e2be
SEUNGHYUN <ysh7733@naver.com>
2023-09-09 23:25:44 +0900

    완벽한 전달 Perfect Forwarding과 std::forward

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,222 +1,40 @@
 #include <iostream>	
-#include <chrono>
-#include <mutex>
-#include <random>
-#include <thread>
-#include <utility>
 #include <vector>
-#include <atomic>
-#include <future>		//	async, future, promise 사용
-#include <numeric>		//	std::inner_product
-#include <execution>	//	parallel execution
+#include <utility> // std::forward
+
 using namespace std;
 
-//	멀티쓰레딩 예제(백터 내적 계산)
+//	완벽한 전달 Perfect Forwarding과 std::forward
 
-mutex mtx;
+struct MyStruct
+{};
 
-void dotProductNaive(const vector<int>& v0, const vector<int>& v1,
-	const unsigned i_start, const unsigned i_end, unsigned long long& sum)
+void func(MyStruct& s) //	L-value refernce
 {
-	for (unsigned i = i_start; i < i_end; ++i)
-	{
-		std::scoped_lock lock(mtx);	//	c++ 17
-
-		sum += v0[i] * v1[i];
-	}
-
-	//cout << "Thread end " << endl;
+	cout << "Pass by L-ref" << endl;
 }
 
-void dotProductLock(const vector<int>& v0, const vector<int>& v1,
-	const unsigned i_start, const unsigned i_end, unsigned long long& sum)
+void func(MyStruct&& s) //	R-value refernce
 {
-	//cout << "Thread start " << endl;
-
-	for (unsigned i = i_start; i < i_end; ++i)
-	{
-		std::scoped_lock lock(mtx);	// c++ 17
-
-		sum += v0[i] * v1[i];
-	}
-
-	//cout << "Thread end " << endl;
+	cout << "Pass by R-ref" << endl;
 }
 
-void dotProductAtomic(const vector<int>& v0, const vector<int>& v1,
-	const unsigned i_start, const unsigned i_end, atomic<unsigned long long> & sum)
-{
-	for (unsigned i = i_start; i < i_end; ++i)
-	{
-		sum += v0[i] * v1[i];
-	}
-}
+//void func(MyStruct s)
+//{
+//	cout << "Pass by Value" << endl;
+//}
 
-auto dotProductFuture(const vector<int>& v0, const vector<int>& v1,
-	const unsigned i_start, const unsigned i_end)
-{
-	int sum = 0; // local sum
-	for (unsigned i = i_start; i < i_end; ++i)
-	{
-		sum += v0[i] * v1[i];
-	}
-
-	return sum;
-}
+//template<typename T>
+//void func_wrapper(T&& t)
+//{
+//	func(std::forward<T>(t));
+//}
 
 int main()
-{	
-	/*
-	v0 = {1, 2, 3}
-	v1 = {4, 5, 6}
-	v0_dot_v1 = 1*4 + 2*5 + 3*6;
-	*/
+{
+	MyStruct s;
 
-	const long long n_data = 100'000'000;
-	const unsigned n_threads = 4;
-
-	//	initialize vectors
-	std::vector<int> v0, v1;
-	v0.reserve(n_data);
-	v1.reserve(n_data);
-
-	//	 랜덤 넘버 생성
-	random_device seed;
-	mt19937 engine(seed());
-
-	uniform_int_distribution<> uniformDist(1, 10);
-
-	for (long long i = 0; i < n_data; ++i)
-	{
-		v0.push_back(uniformDist(engine));
-		v1.push_back(uniformDist(engine));
-	}
-
-	cout << "std::inner_product" << endl;
-	{
-		const auto sta = chrono::steady_clock::now();
-
-		const auto sum = std::inner_product(v0.begin(), v0.end(), v1.begin(), 0ull);
-		//	0ull 넣어주었기 때문에 sum에 마우스 갖다대면 unsigned longlong이 뜨는것임. 
-		//	뒤에 0을 넣으면 sum이 integer로 뜬다. 테스트를 위해 0ull 넣어줌
-
-		const chrono::duration<double> dur = chrono::steady_clock::now() - sta;
-
-		cout << dur.count() << endl;
-		cout << sum << endl;
-		cout << endl;
-	}
-
-	cout << "Naive" << endl;
-	{
-		const auto sta = chrono::steady_clock::now();
-
-		unsigned long long sum = 0;
-
-		vector<thread> threads;
-		threads.resize(n_threads);
-
-		const unsigned n_per_thread = n_data / n_threads; //	assumes remainder = 0
-		for (unsigned t = 0; t < n_threads; ++t)
-			threads[t] = std::thread(dotProductNaive, std::ref(v0), std::ref(v1),
-				t * n_per_thread, (t + 1) * n_per_thread, std::ref(sum));
-
-		for (unsigned t = 0; t < n_threads; ++t)
-			threads[t].join();
-
-		const chrono::duration<double> dur = chrono::steady_clock::now() - sta;
-
-		cout << dur.count() << endl;
-		cout << sum << endl;
-		cout << endl;
-	}
-
-	cout << "Lockguard" << endl;
-	{
-		const auto sta = chrono::steady_clock::now();
-
-		unsigned long long sum = 0;
-
-		vector<thread> threads;
-		threads.resize(n_threads);
-
-		const unsigned n_per_thread = n_data / n_threads; // assumes remainder = 0;
-		for (unsigned t = 0; t < n_threads; ++t)
-			threads[t] = std::thread(dotProductLock, std::ref(v0), std::ref(v1),
-				t * n_per_thread, (t + 1) * n_per_thread, std::ref(sum));
-
-		for (unsigned t = 0; t < n_threads; ++t)
-			threads[t].join();
-
-		const chrono::duration<double> dur = chrono::steady_clock::now() - sta;
-
-		cout << dur.count() << endl;
-		cout << sum << endl;
-		cout << endl;
-	}
-
-	cout << "Atomic" << endl;
-	{
-		const auto sta = chrono::steady_clock::now();
-
-		atomic<unsigned long long> sum = 0;
-
-		vector<thread> threads;
-		threads.resize(n_threads);
-
-		const unsigned n_per_thread = n_data / n_threads;	// assumes remainder = 0;
-		for (unsigned t = 0; t < n_threads; ++t)
-			threads[t] = std::thread(dotProductAtomic, std::ref(v0), std::ref(v1),
-				t * n_per_thread, (t + 1) * n_per_thread, std::ref(sum));
-
-		for (unsigned t = 0; t < n_threads; ++t)
-			threads[t].join();
-		const chrono::duration<double> dur = chrono::steady_clock::now() - sta;
-
-		cout << dur.count() << endl;
-		cout << sum << endl;
-		cout << endl;
-	}
-
-	cout << "Future" << endl;
-	{
-		const auto sta = chrono::steady_clock::now();
-
-		unsigned long long sum = 0;
-
-		vector<std::future<int>> futures;
-		futures.resize(n_threads);
-
-		const unsigned n_per_thread = n_data / n_threads; // assumes remainder = 0
-		for (unsigned t = 0; t < n_threads; ++t)
-			futures[t] = std::async(dotProductFuture, std::ref(v0), std::ref(v1),
-				t * n_per_thread, (t + 1) * n_per_thread);	// sum이 들어올 필요 없다, dotProductFuture에서 local sum 리턴하고 있다.
-
-		for (unsigned t = 0; t < n_threads; ++t)
-			sum += futures[t].get();
-
-		const chrono::duration<double> dur = chrono::steady_clock::now() - sta;
-
-		cout << dur.count() << endl;
-		cout << sum << endl;
-		cout << endl;
-	}
-
-	cout << "std::transform_reduce" << endl;
-	{
-		const auto sta = chrono::steady_clock::now();
-
-		//const auto sum = std::transform_reduce(std::execution::seq, v0.begin(), v0.end(), v1.begin(), 0ull);
-		const auto sum = std::transform_reduce(std::execution::par, v0.begin(), v0.end(), v1.begin(), 0ull);
-
-		const chrono::duration<double> dur = chrono::steady_clock::now() - sta;
-
-		cout << dur.count() << endl;
-		cout << sum << endl;
-		cout << endl;
-	}
-
-	//TODO : use divide and conquer strategy for std::thread
-	//TODO : use promise
-	return 0;
+	/*func(s);
+	func(MyStruct());*/
+	
 }
