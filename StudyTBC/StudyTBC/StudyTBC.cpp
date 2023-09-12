4c867dc195e610760b881dc40a4af8ddd6fc9793
SEUNGHYUN <ysh7733@naver.com>
2023-09-12 16:09:04 +0900

    깃, 깃헙 시작하기 Git Github

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,44 +1,10 @@
 #include <iostream>	
-#include <vector>
-#include <chrono>
-#include <random>
-#include <numeric>	//	std::inner_product
-//	비주얼 스튜디오로 프로파일링 하기	profiling
+
+//	깃, 깃헙 시작하기 Git Github
 
 using namespace std;
-void func1()
-{
-	long long sum = 0;
-	for (unsigned i = 0; i < 1000'000; ++i)
-		sum += i;
-
-	cout << sum << endl;
-}
-
-void func2()
-{
-	long long sum = 0;
-	for (unsigned i = 0; i < 1000'000'000; ++i)
-		sum += i;
-
-	cout << sum << endl;
-}
-
-void run()
-{
-	func1();
-	func2();
-}
 
 int main()
 {
-	//run();
-
-	int *d = new int[10000];
-	float *f = new float[100];
-
-	delete[] d;
-	delete[] f;
-
 	return 0;
 }
