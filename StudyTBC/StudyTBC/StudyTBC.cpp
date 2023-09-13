bd63b386bbe53bc87ecc36ec306f9bbadf06610e
SEUNGHYUN <ysh7733@naver.com>
2023-09-13 12:38:42 +0900

    TCP/IP 네트워킹 맛보기 client

--- StudyTBC/StudyTBC/StudyTBC.cpp
+++ StudyTBC/StudyTBC/StudyTBC.cpp
@@ -1,5 +1,4 @@
 #include <iostream>	
-#include <ctime>
 #include <string>
 #include <boost/asio.hpp>
 
@@ -7,51 +6,40 @@
 //	Boost.asio
 //	Socket IOStream
 
-//	Examples are based on
-//	http://www.boost.org/doc/libs/1_68_0/doc/html/boost_asio/examples.html#boost_asio.examples.iostreams
-using namespace std;
+//	Streaming style
+//	http://www.boost.org/doc/libs/1_68_0/doc/html/boost_asio/overview/networking/iostreams.html
 using boost::asio::ip::tcp;
 
-int main()
+//	127.0.0.1 은 내 컴퓨터다.
+int main(int argc, char* argv[])	// argument로 서버의 주소를 입력 받고있다.
 {
 	try
 	{
-		boost::asio::io_service io_service;
-
-		tcp::endpoint endpoint(tcp::v4(), 13);	//	13 포트 넘버 : 채널 개념
-		tcp::acceptor acceptor(io_service, endpoint);
-
-		std::cout << "Server started" << std::endl;
-
-		for (;;)	// while(true) 무한루프
+		if (argc != 2)
 		{
-			const std::string message_to_send = "Hello From Server";
-
-			boost::asio::ip::tcp::iostream stream;
-
-			std::cout << "check 1" << std::endl;
-
-			boost::system::error_code ec;
-			acceptor.accept(*stream.rdbuf(), ec);
-
-			std::cout << "check 2" << std::endl;
-
-			if (!ec)	// TODO: How to take care of multiple clients? Multi-threading?
-			{
-				//	receive message from client
-				std::string line;
-				std::getline(stream, line);
-				std::cout << line << std::endl;
-
-				//	send message to client
-				stream << message_to_send;
-				stream << std::endl;			//	send std::endl to end getline of client
-			}
+			std::cerr << "Usage: client <host>" << std::endl;
+			return 1;
 		}
+
+		tcp::iostream stream(argv[1], std::to_string(int(13)));	//	port number 13
+		if (!stream)
+		{
+			std::cout << "Unable to connect: " << stream.error().message() << std::endl;
+			return 1;
+		}
+
+		//	send message to server
+		stream << "Hello from client";
+		stream << std::endl;	//	send new-line to end receiver'stream getline
+
+		//	receive message from server
+		std::string line;
+		std::getline(stream, line);
+		std::cout << line << std::endl;
 	}
 	catch (std::exception& e)
 	{
-		std::cerr << e.what() << std::endl;
+		std::cout << "Exception: " << e.what() << std::endl;
 	}
 
 	return 0;
